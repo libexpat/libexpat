db6a73f853760ccab1bc113b7d21c4542b8479ef
James Clark <jjc@jclark.com>
1997-12-11 14:16:35 +0000

    Check for surrogates in NCRs

--- expat/xmltok/xmltok.c
+++ expat/xmltok/xmltok.c
@@ -671,11 +671,19 @@ int XmlParseXmlDecl(int isGeneralTextEntity,
 static
 int checkCharRefNumber(int result)
 {
-  /* FIXME maybe exclude surrogates as well */
-  if ((result < 0x80 && latin1_encoding.type[result] == BT_NONXML)
-      || result == 0xFFFE
-      || result == 0xFFFF)
+  switch (result >> 8) {
+  case 0xD8: case 0xD9: case 0xDA: case 0xDB:
+  case 0xDC: case 0xDD: case 0xDE: case 0xDF:
     return -1;
+  case 0:
+    if (latin1_encoding.type[result] == BT_NONXML)
+      return -1;
+    break;
+  case 0xFF:
+    if (result == 0xFFFE || result == 0xFFFF)
+      return -1;
+    break;
+  }
   return result;
 }
 
