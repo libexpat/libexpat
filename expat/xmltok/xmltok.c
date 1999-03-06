694bd5eee6f23bf80f99d542496ab01fd41b53b2
James Clark <jjc@jclark.com>
1999-03-06 05:42:52 +0000

    Use 0xA instead of \n etc

--- expat/xmltok/xmltok.c
+++ expat/xmltok/xmltok.c
@@ -900,10 +900,10 @@ static
 int isSpace(int c)
 {
   switch (c) {
-  case ' ':
-  case '\r':
-  case '\n':
-  case '\t':
+  case 0x20:
+  case 0xD:
+  case 0xA:
+  case 0x9:	
     return 1;
   }
   return 0;
