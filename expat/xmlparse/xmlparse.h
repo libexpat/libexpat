e2ea2c60e29345f2763511f75198ec954187887d
James Clark <jjc@jclark.com>
1999-08-07 04:06:56 +0000

    Fix comment.

--- expat/xmlparse/xmlparse.h
+++ expat/xmlparse/xmlparse.h
@@ -367,7 +367,7 @@ XML_SetUserData(XML_Parser parser, void *userData);
 #define XML_GetUserData(parser) (*(void **)(parser))
 
 /* This is equivalent to supplying an encoding argument
-to XML_CreateParser. It must not be called after XML_Parse
+to XML_ParserCreate. It must not be called after XML_Parse
 or XML_ParseBuffer. */
 
 int XMLPARSEAPI
