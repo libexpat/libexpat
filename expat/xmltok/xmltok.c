3b0208f5d2b7ec038bfc56e8551f375f5dba02e9
James Clark <jjc@jclark.com>
2000-05-08 04:26:17 +0000

    Warning patch from Nisheeth.

--- expat/xmltok/xmltok.c
+++ expat/xmltok/xmltok.c
@@ -1271,7 +1271,7 @@ XmlInitUnknownEncoding(void *mem,
 {
   int i;
   struct unknown_encoding *e = mem;
-  for (i = 0; i < sizeof(struct normal_encoding); i++)
+  for (i = 0; i < (int)sizeof(struct normal_encoding); i++)
     ((char *)mem)[i] = ((char *)&latin1_encoding)[i];
   for (i = 0; i < 128; i++)
     if (latin1_encoding.type[i] != BT_OTHER
@@ -1389,7 +1389,7 @@ int getEncodingIndex(const char *name)
   int i;
   if (name == 0)
     return NO_ENC;
-  for (i = 0; i < sizeof(encodingNames)/sizeof(encodingNames[0]); i++)
+  for (i = 0; i < (int)(sizeof(encodingNames)/sizeof(encodingNames[0])); i++)
     if (streqci(name, encodingNames[i]))
       return i;
   return UNKNOWN_ENC;
