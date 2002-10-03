7edd98f770f1f9c41c8658db57ba32f5ed9fe9db
Karl Waclawek <kwaclaw@users.sourceforge.net>
2002-10-03 19:41:26 +0000

    Applied patch #618199.

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -1710,11 +1710,17 @@ storeRawNames(XML_Parser parser)
       char *temp = REALLOC(tag->buf, bufSize);
       if (temp == NULL)
         return XML_FALSE;
-      /* if tag->name.str points to tag->buf (i.e. when namespace
+      /* if tag->name.str points to tag->buf (only when namespace
          processing is off) then we have to update it 
       */
       if (tag->name.str == (XML_Char *)tag->buf)
         tag->name.str = (XML_Char *)temp;
+      /* if tag->name.localPart is set (when namespace processing is on)
+         then update it as well, since it will always point into tag-buf
+      */
+      if (tag->name.localPart)
+        tag->name.localPart = (XML_Char *)temp + (tag->name.localPart -
+                                                  (XML_Char *)tag->buf);
       tag->buf = temp;
       tag->bufEnd = temp + bufSize;
       rawNameBuf = temp + nameLen;
