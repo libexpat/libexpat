23e56eae281a7fdc27f7d2a95d79cddfdc12a3d4
Rhodri James <rhodri@kynesim.co.uk>
2017-04-25 17:44:00 +0100

    Validate parser parameter to XML_SetSkippedEntityHandler

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -1568,7 +1568,8 @@ void XMLCALL
 XML_SetSkippedEntityHandler(XML_Parser parser,
                             XML_SkippedEntityHandler handler)
 {
-  skippedEntityHandler = handler;
+  if (parser != NULL)
+    skippedEntityHandler = handler;
 }
 
 void XMLCALL
