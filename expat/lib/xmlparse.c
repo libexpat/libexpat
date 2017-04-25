2825dc0baeee8dca4f344070c982f2c94e5d2810
Rhodri James <rhodri@kynesim.co.uk>
2017-04-25 17:20:31 +0100

    Validate parser parameter to XML_SetDefaultHandler

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -1455,6 +1455,8 @@ void XMLCALL
 XML_SetDefaultHandler(XML_Parser parser,
                       XML_DefaultHandler handler)
 {
+  if (parser == NULL)
+    return;
   defaultHandler = handler;
   defaultExpandInternalEntities = XML_FALSE;
 }
