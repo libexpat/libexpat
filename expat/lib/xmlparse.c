2315924f2a875b56d40af7b3fc8f6abdb60ef3dc
Rhodri James <rhodri@kynesim.co.uk>
2017-04-25 17:27:00 +0100

    Validate parser parameter to XML_SetDoctypeDeclHandler

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -1476,6 +1476,8 @@ XML_SetDoctypeDeclHandler(XML_Parser parser,
                           XML_StartDoctypeDeclHandler start,
                           XML_EndDoctypeDeclHandler end)
 {
+  if (parser == NULL)
+    return;
   startDoctypeDeclHandler = start;
   endDoctypeDeclHandler = end;
 }
