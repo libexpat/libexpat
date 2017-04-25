62befbddcdd823089f211a040b4b526b91d44823
Rhodri James <rhodri@kynesim.co.uk>
2017-04-25 17:45:08 +0100

    Validate parser parameter to XML_SetUnknownEncodingHandler

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -1577,6 +1577,8 @@ XML_SetUnknownEncodingHandler(XML_Parser parser,
                               XML_UnknownEncodingHandler handler,
                               void *data)
 {
+  if (parser == NULL)
+    return;
   unknownEncodingHandler = handler;
   unknownEncodingHandlerData = data;
 }
