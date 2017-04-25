5c374aa91d43ffe80cbe95d41efc099694ac3ee5
Rhodri James <rhodri@kynesim.co.uk>
2017-04-25 17:15:57 +0100

    Validate parser parameter to XML_SetCdataSectionHandler

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -1431,6 +1431,8 @@ XML_SetCdataSectionHandler(XML_Parser parser,
                            XML_StartCdataSectionHandler start,
                            XML_EndCdataSectionHandler end)
 {
+  if (parser == NULL)
+    return;
   startCdataSectionHandler = start;
   endCdataSectionHandler = end;
 }
