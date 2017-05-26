69b864b9b1c483bdcf796971a68bf2c325ed58e0
Rhodri James <rhodri@kynesim.co.uk>
2017-05-26 17:36:00 +0100

    Test XML declaration with missing value for 'standalone' attr

--- expat/tests/runtests.c
+++ expat/tests/runtests.c
@@ -1342,6 +1342,15 @@ START_TEST(test_xmldecl_missing_attr)
 }
 END_TEST
 
+START_TEST(test_xmldecl_missing_value)
+{
+    expect_failure("<?xml version='1.0' encoding='us-ascii' standalone?>\n"
+                   "<doc/>",
+                   XML_ERROR_XML_DECL,
+                   "Failed to report missing attribute value");
+}
+END_TEST
+
 /* Regression test for SF bug #584832. */
 static int XMLCALL
 UnknownEncodingHandler(void *UNUSED_P(data),const XML_Char *encoding,XML_Encoding *info)
@@ -10403,6 +10412,7 @@ make_suite(void)
     tcase_add_test(tc_basic, test_xmldecl_misplaced);
     tcase_add_test(tc_basic, test_xmldecl_invalid);
     tcase_add_test(tc_basic, test_xmldecl_missing_attr);
+    tcase_add_test(tc_basic, test_xmldecl_missing_value);
     tcase_add_test(tc_basic, test_unknown_encoding_internal_entity);
     tcase_add_test(tc_basic, test_unrecognised_encoding_internal_entity);
     tcase_add_test(tc_basic,
