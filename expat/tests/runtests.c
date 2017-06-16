61db6a8a936a51ede918fc6483e1ce37bb0684eb
Rhodri James <rhodri@kynesim.co.uk>
2017-06-16 17:46:08 +0100

    Test document name of "1*" is rejected in a doctype declaration

--- expat/tests/runtests.c
+++ expat/tests/runtests.c
@@ -6288,6 +6288,17 @@ START_TEST(test_bad_doctype_plus)
 }
 END_TEST
 
+START_TEST(test_bad_doctype_star)
+{
+    const char *text =
+        "<!DOCTYPE 1* [ <!ENTITY foo 'bar'> ]>\n"
+        "<1*>&foo;</1*>";
+
+    expect_failure(text, XML_ERROR_INVALID_TOKEN,
+                   "'+' in document name not faulted");
+}
+END_TEST
+
 /*
  * Namespaces tests.
  */
@@ -11756,6 +11767,7 @@ make_suite(void)
     tcase_add_test(tc_basic, test_bad_doctype);
     tcase_add_test(tc_basic, test_bad_doctype_utf16);
     tcase_add_test(tc_basic, test_bad_doctype_plus);
+    tcase_add_test(tc_basic, test_bad_doctype_star);
 
     suite_add_tcase(s, tc_namespace);
     tcase_add_checked_fixture(tc_namespace,
