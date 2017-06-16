760077feebc399998c66ec0e7c023865c2f21151
Sebastian Pipping <sebastian@pipping.org>
2017-06-16 20:47:55 +0200

    xmlparse.c: Polish ENTROPY_DEBUG
    The cast fixes a compile warning.

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -806,8 +806,8 @@ ENTROPY_DEBUG(const char * label, unsigned long entropy) {
   if (EXPAT_ENTROPY_DEBUG && ! strcmp(EXPAT_ENTROPY_DEBUG, "1")) {
     fprintf(stderr, "Entropy: %s --> 0x%0*lx (%lu bytes)\n",
         label,
-        (int)sizeof(unsigned long) * 2, entropy,
-        sizeof(unsigned long));
+        (int)sizeof(entropy) * 2, entropy,
+        (unsigned long)sizeof(entropy));
   }
   return entropy;
 }
