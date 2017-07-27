88436b3723411913563df9c5ebb6f5c999b2a589
Rhodri James <rhodri@kynesim.co.uk>
2017-07-27 14:12:48 +0100

    Make test_alloc_attribute_whitespace() robust vs allocation changes

--- expat/tests/runtests.c
+++ expat/tests/runtests.c
@@ -9607,26 +9607,16 @@ START_TEST(test_alloc_attribute_whitespace)
 {
     const char *text = "<doc a=' '></doc>";
     int i;
-#define MAX_ALLOC_COUNT 10
-    int repeat = 0;
+#define MAX_ALLOC_COUNT 15
 
     for (i = 0; i < MAX_ALLOC_COUNT; i++) {
-        /* Repeat some counts to defeat cached allocations */
-        if (i == 3 && repeat == 1) {
-            i -= 2;
-            repeat++;
-        }
-        else if ((i == 2 &&
-                  (repeat == 0 || repeat == 2 || repeat == 3)) ||
-                 (i == 3 && repeat == 4)) {
-            i--;
-            repeat++;
-        }
         allocation_count = i;
         if (_XML_Parse_SINGLE_BYTES(parser, text, strlen(text),
                                     XML_TRUE) != XML_STATUS_ERROR)
             break;
-        XML_ParserReset(parser, NULL);
+        /* See comment in test_alloc_parse_xdecl() */
+        alloc_teardown();
+        alloc_setup();
     }
     if (i == 0)
         fail("Parse succeeded despite failing allocator");
