4dcb57a98ea8a807850d373867d28a46b1d33561
Rhodri James <rhodri@kynesim.co.uk>
2017-07-27 13:55:43 +0100

    make test_alloc_nested_groups() robust vs allocation pattern changes

--- expat/tests/runtests.c
+++ expat/tests/runtests.c
@@ -9352,15 +9352,9 @@ START_TEST(test_alloc_nested_groups)
         "<doc><e/></doc>";
     CharData storage;
     int i;
-#define MAX_ALLOC_COUNT 10
-    int repeat = 0;
+#define MAX_ALLOC_COUNT 20
 
     for (i = 0; i < MAX_ALLOC_COUNT; i++) {
-        /* Repeat some counts to allow for cached allocations */
-        if ((i == 2 && repeat < 3) || (i == 3 && repeat == 3)) {
-            i--;
-            repeat++;
-        }
         allocation_count = i;
         CharData_Init(&storage);
         XML_SetElementDeclHandler(parser, dummy_element_decl_handler);
@@ -9370,7 +9364,9 @@ START_TEST(test_alloc_nested_groups)
         if (_XML_Parse_SINGLE_BYTES(parser, text, strlen(text),
                                     XML_TRUE) != XML_STATUS_ERROR)
             break;
-        XML_ParserReset(parser, NULL);
+        /* See comment in test_alloc_parse_xdecl() */
+        alloc_teardown();
+        alloc_setup();
     }
 
     if (i == 0)
