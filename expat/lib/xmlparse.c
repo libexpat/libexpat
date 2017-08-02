b5b2009b005279ae04a0fb920ba882c17aaf15fa
Rhodri James <rhodri@kynesim.co.uk>
2017-08-02 17:35:25 +0100

    Keep allocDefaultAtts consistent if allocating defaultAtts fails.
    See issue #8

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -6029,8 +6029,10 @@ defineAttribute(ELEMENT_TYPE *type, ATTRIBUTE_ID *attId, XML_Bool isCdata,
       type->allocDefaultAtts = 8;
       type->defaultAtts = (DEFAULT_ATTRIBUTE *)MALLOC(type->allocDefaultAtts
                             * sizeof(DEFAULT_ATTRIBUTE));
-      if (!type->defaultAtts)
+      if (!type->defaultAtts) {
+        type->allocDefaultAtts = 0;
         return 0;
+      }
     }
     else {
       DEFAULT_ATTRIBUTE *temp;
