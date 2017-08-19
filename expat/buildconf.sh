libexpat/libexpat

commit d4390f34f3890e969137605adcbac4ed571e00e4

Replace handwritten Makefile with GNU Automake

expat/buildconf.sh
@@ -1,30 +1,2 @@
 #! /bin/sh
-
-#--------------------------------------------------------------------------
-# autoconf 2.58 or newer
-#
-ac_version="`${AUTOCONF:-autoconf} --version 2> /dev/null | head -1 | sed -e 's/^[^0-9]*//' -e 's/[a-z]* *$//'`"
-if test -z "$ac_version"; then
-  echo "ERROR: autoconf not found."
-  echo "       You need autoconf version 2.58 or newer installed."
-  exit 1
-fi
-IFS=.; set $ac_version; IFS=' '
-if test "$1" = "2" -a "$2" -lt "58" || test "$1" -lt "2"; then
-  echo "ERROR: autoconf version $ac_version found."
-  echo "       You need autoconf version 2.58 or newer installed."
-  exit 1
-fi
-
-echo "Creating configure ..."
-${AUTORECONF:-autoreconf} -fvi
-
-echo "Creating conftools/install-sh ..."
-# .. for configure, despite not using automake
-automake --add-missing 2>/dev/null || true
-
-# toss this; it gets created by autoconf on some systems
-rm -rf autom4te*.cache
-
-# exit with the right value, so any calling script can continue
-exit 0
+exec autoreconf -i

expat/.gitignore
@@ -9,6 +9,7 @@ CTestTestfile.cmake
 install_manifest.txt
 Makefile
 .deps
+Makefile.in
 .libs
 *.la
 configure

expat/CMakeLists.txt
@@ -161,7 +161,7 @@ if(BUILD_tools)
             set(make_command "make")
         endif()
 
-        add_custom_command(TARGET expat PRE_BUILD COMMAND "${make_command}" -C "${PROJECT_SOURCE_DIR}/doc" xmlwf.1 "DOCBOOK_TO_MAN=${DOCBOOK_TO_MAN}")
+        add_custom_command(TARGET expat PRE_BUILD COMMAND "${make_command}" -f "${PROJECT_SOURCE_DIR}/doc/doc.mk" -C "${PROJECT_SOURCE_DIR}/doc" xmlwf.1 "DOCBOOK_TO_MAN=${DOCBOOK_TO_MAN}")
         expat_install(FILES "${PROJECT_SOURCE_DIR}/doc/xmlwf.1" DESTINATION share/man/man1)
     endif()
 endif()

expat/MANIFEST
@@ -1,90 +1,0 @@
-AUTHORS
-doc/expat.png
-doc/reference.html
-doc/style.css
-doc/valid-xhtml10.png
-doc/xmlwf.1
-doc/xmlwf.xml
-CMakeLists.txt
-CMake.README
-COPYING
-Changes
-ConfigureChecks.cmake
-MANIFEST
-Makefile.in
-README.md
-configure
-configure.ac
-expat_config.h.in
-expat_config.h.cmake
-expat.pc.in
-aclocal.m4
-run.sh.in
-conftools/PrintPath
-conftools/ac_c_bigendian_cross.m4
-conftools/expat.m4
-conftools/get-version.sh
-conftools/mkinstalldirs
-conftools/config.guess
-conftools/config.sub
-conftools/install-sh
-conftools/ltmain.sh
-m4/libtool.m4
-m4/ltversion.m4
-m4/ltoptions.m4
-m4/ltsugar.m4
-m4/lt~obsolete.m4
-examples/elements.c
-examples/outline.c
-lib/ascii.h
-lib/asciitab.h
-lib/expat.h
-lib/expat_external.h
-lib/iasciitab.h
-lib/internal.h
-lib/latin1tab.h
-lib/libexpat.def
-lib/libexpatw.def
-lib/loadlibrary.c
-lib/nametab.h
-lib/siphash.h
-lib/utf8tab.h
-lib/winconfig.h
-lib/xmlparse.c
-lib/xmlrole.c
-lib/xmlrole.h
-lib/xmltok.c
-lib/xmltok.h
-lib/xmltok_impl.c
-lib/xmltok_impl.h
-lib/xmltok_ns.c
-tests/benchmark/README.txt
-tests/benchmark/benchmark.c
-tests/README.txt
-tests/chardata.c
-tests/chardata.h
-tests/memcheck.c
-tests/memcheck.h
-tests/minicheck.c
-tests/minicheck.h
-tests/runtests.c
-tests/runtestspp.cpp
-tests/xmltest.sh
-win32/MANIFEST.txt
-win32/README.txt
-win32/expat.iss
-xmlwf/codepage.c
-xmlwf/codepage.h
-xmlwf/ct.c
-xmlwf/filemap.h
-xmlwf/readfilemap.c
-xmlwf/unixfilemap.c
-xmlwf/win32filemap.c
-xmlwf/xmlfile.c
-xmlwf/xmlfile.h
-xmlwf/xmlmime.c
-xmlwf/xmlmime.h
-xmlwf/xmltchar.h
-xmlwf/xmlurl.h
-xmlwf/xmlwf.c
-xmlwf/xmlwin32url.cxx

expat/Makefile.am
@@ -1,0 +1,121 @@
+#
+#                          __  __            _
+#                       ___\ \/ /_ __   __ _| |_
+#                      / _ \\  /| '_ \ / _` | __|
+#                     |  __//  \| |_) | (_| | |_
+#                      \___/_/\_\ .__/ \__,_|\__|
+#                               |_| XML parser
+#
+# Copyright (c) 2017 Expat development team
+# Licensed under the MIT license:
+#
+# Permission is  hereby granted,  free of charge,  to any  person obtaining
+# a  copy  of  this  software   and  associated  documentation  files  (the
+# "Software"),  to  deal in  the  Software  without restriction,  including
+# without  limitation the  rights  to use,  copy,  modify, merge,  publish,
+# distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons  to whom  the Software  is  furnished to  do so,  subject to  the
+# following conditions:
+#
+# The above copyright  notice and this permission notice  shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE  SOFTWARE  IS  PROVIDED  "AS  IS",  WITHOUT  WARRANTY  OF  ANY  KIND,
+# EXPRESS  OR IMPLIED,  INCLUDING  BUT  NOT LIMITED  TO  THE WARRANTIES  OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+# NO EVENT SHALL THE AUTHORS OR  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR  OTHER LIABILITY, WHETHER  IN AN  ACTION OF CONTRACT,  TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+# USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+AUTOMAKE_OPTIONS = \
+    dist-bzip2 \
+    foreign \
+    no-dist-gzip \
+    subdir-objects
+
+ACLOCAL_AMFLAGS = -I m4
+LIBTOOLFLAGS = --verbose
+
+SUBDIRS = lib examples tests  # lib goes first to build first
+if WITH_XMLWF
+SUBDIRS += xmlwf doc
+endif
+
+pkgconfig_DATA = expat.pc
+pkgconfigdir = $(libdir)/pkgconfig
+
+
+EXTRA_DIST = \
+    conftools/expat.m4 \
+    conftools/get-version.sh \
+    conftools/PrintPath \
+    \
+    win32/expat.iss \
+    win32/MANIFEST.txt \
+    win32/README.txt \
+    \
+    Changes \
+    CMakeLists.txt \
+    CMake.README \
+    ConfigureChecks.cmake \
+    expat_config.h.cmake \
+    README.md \
+    test-driver-wrapper.sh
+
+
+.PHONY: buildlib
+buildlib:
+	@echo 'ERROR: Running "make buildlib LIBRARY=libexpatw.la"' >&2
+	@echo 'ERROR: is no longer supported.  INSTEAD please:' >&2
+	@echo 'ERROR:' >&2
+	@echo 'ERROR:  * Mass-patch Makefile.am, e.g.' >&2
+	@echo 'ERROR:    # find -name Makefile.am -exec sed \' >&2
+	@echo 'ERROR:          -e "s,libexpat\.la,libexpatw\.la," \' >&2
+	@echo 'ERROR:          -e "s,libexpat_la,libexpatw_la," \' >&2
+	@echo 'ERROR:          -i {} +' >&2
+	@echo 'ERROR:' >&2
+	@echo 'ERROR:  * Run automake to re-generate Makefile.in files' >&2
+	@echo 'ERROR:' >&2
+	@echo 'ERROR:  * Use "./configure --without-xmlwf" and/or' >&2
+	@echo 'ERROR:    "make -C lib all install" to bypass compilation' >&2
+	@echo 'ERROR:    of xmlwf (e.g. with -DXML_UNICODE)' >&2
+	@echo 'ERROR:' >&2
+	@false
+
+
+.PHONY: run-benchmark
+run-benchmark:
+	$(MAKE) -C tests/benchmark
+	tests/benchmark/benchmark@EXEEXT@ -n $(top_srcdir)/../testdata/largefiles/recset.xml 65535 3
+
+tests/xmlts.zip:
+	if test "$(XMLTS_ZIP)" = ""; then \
+		wget --output-document=tests/xmlts.zip \
+			https://www.w3.org/XML/Test/xmlts20080827.zip; \
+	else \
+		cp $(XMLTS_ZIP) tests/xmlts.zip; \
+	fi
+
+tests/xmlconf: tests/xmlts.zip
+	cd tests && unzip -q xmlts.zip
+
+.PHONY: run-xmltest
+run-xmltest: tests/xmlconf
+if WITH_XMLWF
+	$(MAKE) -C xmlwf
+	tests/xmltest.sh "$(PWD)/run.sh $(PWD)/xmlwf/xmlwf@EXEEXT@" 2>&1 | tee tests/xmltest.log
+	dos2unix tests/xmltest.log
+	diff -u tests/xmltest.log.expected tests/xmltest.log
+else
+	@echo 'ERROR: xmlwf is needed for "make run-xmltest".' >&2
+	@echo 'ERROR: Please re-configure without --without-xmlwf.' >&2
+	@false
+endif
+
+.PHONY: qa
+qa:
+	./qa.sh address
+	./qa.sh memory
+	./qa.sh undefined
+	./qa.sh coverage

expat/Makefile.in
@@ -1,239 +1,0 @@
-################################################################
-# Process this file with top-level configure script to produce Makefile
-#
-# Copyright 2000 Clark Cooper
-#
-#  This file is part of EXPAT.
-#
-#  EXPAT is free software; you can redistribute it and/or modify it
-#  under the terms of the License (based on the MIT/X license) contained
-#  in the file COPYING that comes with this distribution.
-#
-# EXPAT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN EXPAT.
-#
-
-SHELL = @SHELL@
-
-srcdir = @srcdir@
-top_srcdir = @top_srcdir@
-VPATH = @srcdir@
-
-prefix = @prefix@
-exec_prefix = @exec_prefix@
-
-bindir = @bindir@
-libdir = @libdir@
-includedir = @includedir@
-man1dir = @mandir@/man1
-pkgconfigdir = $(libdir)/pkgconfig
-
-top_builddir = .
-
-
-INSTALL = @INSTALL@
-INSTALL_PROGRAM = @INSTALL_PROGRAM@
-INSTALL_DATA = @INSTALL_DATA@
-mkinstalldirs = $(SHELL) $(top_srcdir)/conftools/mkinstalldirs
-
-MANFILE = $(srcdir)/doc/xmlwf.1
-APIHEADER = $(srcdir)/lib/expat.h $(srcdir)/lib/expat_external.h expat_config.h
-LIBRARY = libexpat.la
-
-DESTDIR = $(INSTALL_ROOT)
-
-XMLWF_ENABLED = test @with_xmlwf@ = yes
-
-
-default:  buildlib
-	if $(XMLWF_ENABLED) ; then $(MAKE) xmlwf/xmlwf@EXEEXT@ ; fi
-
-buildlib: $(LIBRARY) expat.pc
-
-all: $(LIBRARY) expat.pc examples/elements examples/outline $(MANFILE)
-	if $(XMLWF_ENABLED) ; then $(MAKE) xmlwf/xmlwf@EXEEXT@ ; fi
-
-clean:
-	cd lib && rm -f $(LIBRARY) *.@OBJEXT@ *.lo && rm -rf .libs _libs
-	cd xmlwf && rm -f xmlwf *.@OBJEXT@ *.lo && rm -rf .libs _libs
-	cd examples && rm -f elements outline *.@OBJEXT@ *.lo && rm -rf .libs _libs
-	cd tests && rm -rf .libs runtests@EXEEXT@ runtests.@OBJEXT@ runtestspp@EXEEXT@ runtestspp.@OBJEXT@
-	cd tests && rm -f chardata.@OBJEXT@ memcheck.@OBJEXT@ minicheck.@OBJEXT@
-	rm -rf .libs libexpat.la
-	rm -f examples/core tests/core xmlwf/core
-
-clobber: clean
-
-distclean: clean
-	rm -f expat_config.h config.status config.log config.cache libtool
-	rm -f Makefile expat.pc
-
-extraclean: distclean
-	rm -f expat_config.h.in configure
-	rm -f aclocal.m4 m4/*
-	rm -f conftools/ltmain.sh conftools/install-sh conftools/config.guess conftools/config.sub
-
-check: tests/runtests@EXEEXT@ tests/runtestspp@EXEEXT@
-	./run.sh tests/runtests@EXEEXT@
-	./run.sh tests/runtestspp@EXEEXT@
-
-$(MANFILE):
-	$(MAKE) -C doc xmlwf.1
-
-install: installlib
-	if $(XMLWF_ENABLED) ; then $(MAKE) installman installxmlwf ; fi
-
-.PHONY: installman
-installman: $(MANFILE)
-	$(mkinstalldirs) $(DESTDIR)$(man1dir)
-	$(INSTALL_DATA) $(MANFILE) $(DESTDIR)$(man1dir)
-
-.PHONY: installxmlwf
-installxmlwf: xmlwf/xmlwf@EXEEXT@ installlib
-	$(mkinstalldirs) $(DESTDIR)$(bindir)
-	$(LIBTOOL) --mode=install $(INSTALL_PROGRAM) xmlwf/xmlwf@EXEEXT@ $(DESTDIR)$(bindir)/xmlwf
-
-installlib: $(LIBRARY) $(APIHEADER) expat.pc
-	$(mkinstalldirs) $(DESTDIR)$(libdir) $(DESTDIR)$(includedir) $(DESTDIR)$(pkgconfigdir)
-	$(LIBTOOL) --mode=install $(INSTALL) $(LIBRARY) $(DESTDIR)$(libdir)/$(LIBRARY)
-	for FN in $(APIHEADER) ; do $(INSTALL_DATA) $$FN $(DESTDIR)$(includedir) ; done
-	$(INSTALL_DATA) expat.pc $(DESTDIR)$(pkgconfigdir)/expat.pc
-
-uninstall: uninstalllib uninstallman uninstallxmlwf
-
-uninstalllib:
-	$(LIBTOOL) --mode=uninstall rm -f $(DESTDIR)$(libdir)/$(LIBRARY)
-	rm -f $(DESTDIR)$(includedir)/expat.h
-	rm -f $(DESTDIR)$(includedir)/expat_external.h
-	rm -f $(DESTDIR)$(pkgconfigdir)/expat.pc
-
-.PHONY: uninstallman
-uninstallman:
-	rm -f $(DESTDIR)$(man1dir)/xmlwf.1
-
-.PHONY: uninstallxmlwf
-uninstallxmlwf:
-	$(LIBTOOL) --mode=uninstall rm -f $(DESTDIR)$(bindir)/xmlwf@EXEEXT@
-
-# for VPATH builds (invoked by configure)
-mkdir-init:
-	@for d in lib xmlwf examples tests ; do \
-		(mkdir $$d 2> /dev/null || test 1) ; \
-	done
-
-CC = @CC@
-CXX = @CXX@
-LIBTOOL = @LIBTOOL@
-
-INCLUDES = -I$(srcdir)/lib -I.
-LDFLAGS = @LDFLAGS@
-CPPFLAGS = @CPPFLAGS@ -DHAVE_EXPAT_CONFIG_H
-CFLAGS = @CFLAGS@
-CXXFLAGS = @CXXFLAGS@
-VSNFLAG = -version-info @LIBCURRENT@:@LIBREVISION@:@LIBAGE@
-
-### autoconf this?
-LTFLAGS = --verbose
-
-COMPILE = $(CC) $(INCLUDES) $(CFLAGS) $(DEFS) $(CPPFLAGS)
-CXXCOMPILE = $(CXX) $(INCLUDES) $(CXXFLAGS) $(DEFS) $(CPPFLAGS)
-LTCOMPILE = $(LIBTOOL) $(LTFLAGS) --mode=compile $(COMPILE)
-LINK_LIB = $(LIBTOOL) $(LTFLAGS) --mode=link $(COMPILE) -no-undefined $(VSNFLAG) -rpath $(libdir) $(LDFLAGS) @LIBS@ -o $@
-LINK_EXE = $(LIBTOOL) $(LTFLAGS) --mode=link $(COMPILE) $(LDFLAGS) -o $@
-LINK_CXX_EXE = $(LIBTOOL) $(LTFLAGS) --mode=link $(CXXCOMPILE) $(LDFLAGS) -o $@
-
-LIB_OBJS = lib/loadlibrary.lo lib/xmlparse.lo lib/xmltok.lo lib/xmlrole.lo
-$(LIBRARY): $(LIB_OBJS)
-	$(LINK_LIB) $(LIB_OBJS)
-
-expat.pc: $(top_builddir)/config.status
-	cd $(top_builddir) && $(SHELL) ./config.status $@
-
-lib/xmlparse.lo: lib/xmlparse.c lib/expat.h lib/siphash.h lib/xmlrole.h lib/xmltok.h \
-	$(top_builddir)/expat_config.h lib/expat_external.h lib/internal.h
-
-lib/loadlibrary.lo: lib/loadlibrary.c
-
-lib/xmlrole.lo: lib/xmlrole.c lib/ascii.h lib/xmlrole.h \
-	$(top_builddir)/expat_config.h lib/expat_external.h lib/internal.h
-
-lib/xmltok.lo: lib/xmltok.c lib/xmltok_impl.c lib/xmltok_ns.c \
-	lib/ascii.h lib/asciitab.h lib/iasciitab.h lib/latin1tab.h \
-	lib/nametab.h lib/utf8tab.h lib/xmltok.h lib/xmltok_impl.h \
-	$(top_builddir)/expat_config.h lib/expat_external.h lib/internal.h
-
-
-XMLWF_OBJS = xmlwf/xmlwf.@OBJEXT@ xmlwf/xmlfile.@OBJEXT@ xmlwf/codepage.@OBJEXT@ xmlwf/@FILEMAP@.@OBJEXT@
-xmlwf/xmlwf.@OBJEXT@: xmlwf/xmlwf.c
-xmlwf/xmlfile.@OBJEXT@: xmlwf/xmlfile.c
-xmlwf/codepage.@OBJEXT@: xmlwf/codepage.c
-xmlwf/@FILEMAP@.@OBJEXT@: xmlwf/@FILEMAP@.c xmlwf/filemap.h
-xmlwf/xmlwf@EXEEXT@: $(XMLWF_OBJS) $(LIBRARY)
-	$(LINK_EXE) $(XMLWF_OBJS) $(LIBRARY)
-
-examples/elements.@OBJEXT@: examples/elements.c
-examples/elements: examples/elements.@OBJEXT@ $(LIBRARY)
-	$(LINK_EXE) examples/elements.@OBJEXT@ $(LIBRARY)
-
-examples/outline.@OBJEXT@: examples/outline.c
-examples/outline: examples/outline.@OBJEXT@ $(LIBRARY)
-	$(LINK_EXE) examples/outline.@OBJEXT@ $(LIBRARY)
-
-tests/chardata.@OBJEXT@: tests/chardata.c tests/chardata.h
-tests/minicheck.@OBJEXT@: tests/minicheck.c tests/minicheck.h
-tests/memcheck.@OBJEXT@: tests/memcheck.c tests/memcheck.h
-tests/runtests.@OBJEXT@: tests/runtests.c tests/chardata.h tests/memcheck.h lib/siphash.h
-tests/runtests@EXEEXT@: tests/runtests.@OBJEXT@ tests/chardata.@OBJEXT@ tests/minicheck.@OBJEXT@ tests/memcheck.@OBJEXT@ $(LIBRARY)
-	$(LINK_EXE) tests/runtests.@OBJEXT@ tests/chardata.@OBJEXT@ tests/minicheck.@OBJEXT@ tests/memcheck.@OBJEXT@ $(LIBRARY)
-tests/runtestspp.@OBJEXT@: tests/runtestspp.cpp tests/runtests.c tests/chardata.h tests/memcheck.h
-tests/runtestspp@EXEEXT@: tests/runtestspp.@OBJEXT@ tests/chardata.@OBJEXT@ tests/minicheck.@OBJEXT@ tests/memcheck.@OBJEXT@ $(LIBRARY)
-	$(LINK_CXX_EXE) tests/runtestspp.@OBJEXT@ tests/chardata.@OBJEXT@ tests/minicheck.@OBJEXT@ tests/memcheck.@OBJEXT@ $(LIBRARY)
-
-tests/benchmark/benchmark.@OBJEXT@: tests/benchmark/benchmark.c
-tests/benchmark/benchmark: tests/benchmark/benchmark.@OBJEXT@ $(LIBRARY)
-	$(LINK_EXE) tests/benchmark/benchmark.@OBJEXT@ $(LIBRARY)
-
-run-benchmark: tests/benchmark/benchmark
-	tests/benchmark/benchmark@EXEEXT@ -n $(top_srcdir)/../testdata/largefiles/recset.xml 65535 3
-
-tests/xmlts.zip:
-	if test "$(XMLTS_ZIP)" = ""; then \
-		wget --output-document=tests/xmlts.zip \
-			https://www.w3.org/XML/Test/xmlts20080827.zip; \
-	else \
-		cp $(XMLTS_ZIP) tests/xmlts.zip; \
-	fi
-
-tests/xmlconf: tests/xmlts.zip
-	cd tests && unzip -q xmlts.zip
-
-run-xmltest: xmlwf/xmlwf@EXEEXT@ tests/xmlconf
-	tests/xmltest.sh "$(PWD)/run.sh $(PWD)/xmlwf/xmlwf@EXEEXT@" 2>&1 | tee tests/xmltest.log
-	dos2unix tests/xmltest.log
-	diff -u tests/xmltest.log.expected tests/xmltest.log
-
-.PHONY: qa
-qa:
-	./qa.sh address
-	./qa.sh memory
-	./qa.sh undefined
-	./qa.sh coverage
-
-.SUFFIXES: .c .cpp .lo .@OBJEXT@
-
-.cpp.@OBJEXT@:
-	$(CXXCOMPILE) -o $@ -c $<
-.c.@OBJEXT@:
-	$(COMPILE) -o $@ -c $<
-.c.lo:
-	$(LTCOMPILE) -o $@ -c $<
-
-.PHONY: buildlib all \
-	clean distclean extraclean maintainer-clean \
-	dist distdir \
-	install uninstall

expat/configure.ac
@@ -31,6 +31,7 @@ undefine([expat_version])
 
 AC_CONFIG_SRCDIR(Makefile.in)
 AC_CONFIG_AUX_DIR(conftools)
+AM_INIT_AUTOMAKE
 AC_CONFIG_MACRO_DIR([m4])
 
 
@@ -49,6 +50,7 @@ LIBCURRENT=7   # sync
 LIBREVISION=5  # with
 LIBAGE=6       # CMakeLists.txt!
 
+CPPFLAGS="${CPPFLAGS} -DHAVE_EXPAT_CONFIG_H"
 AC_CONFIG_HEADER(expat_config.h)
 
 sinclude(conftools/ac_c_bigendian_cross.m4)
@@ -102,7 +104,7 @@ AC_CHECK_FUNCS(memmove bcopy)
 
 AC_ARG_WITH([xmlwf], [
 AS_HELP_STRING([--without-xmlwf], [do not build xmlwf])], [], [with_xmlwf=yes])
-AC_SUBST(with_xmlwf)
+AM_CONDITIONAL([WITH_XMLWF], [test x${with_xmlwf} = xyes])
 
 
 AC_ARG_WITH([libbsd], [
@@ -224,11 +226,13 @@ AS_IF([test "x${enable_xml_context}" != "xno"], [
 ])
 
 AC_CONFIG_FILES([Makefile expat.pc])
+AC_CONFIG_FILES([
+  doc/Makefile
+  examples/Makefile
+  lib/Makefile
+  tests/Makefile
+  tests/benchmark/Makefile
+  xmlwf/Makefile
+])
 AC_CONFIG_FILES([run.sh], [chmod +x run.sh])
 AC_OUTPUT
-
-abs_srcdir="`cd $srcdir && pwd`"
-abs_builddir="`pwd`"
-if test "$abs_srcdir" != "$abs_builddir"; then
-  make mkdir-init
-fi

expat/conftools/.gitignore
@@ -1,6 +1,10 @@
+compile
 config.guess
 config.sub
+depcomp
 install-sh
 libtool.m4
 ltconfig
 ltmain.sh
+missing
+test-driver

commit d0aa68d90197b46b28712473c6e132d1d954421a

buildconf.sh: Make sure that configure has latest version (#65)
Regression from d4390f34f3890e969137605adcbac4ed571e00e4

expat/buildconf.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-exec autoreconf -i
+exec autoreconf -i -f