5dbd329aee2e40a29edc7da0a0e72ccc9550ff72
Rhodri James <rhodri@kynesim.co.uk>
2017-08-22 15:13:26 +0100

    Fix test_ignore_section_utf16() for UTF-16 builds

--- expat/tests/runtests.c
+++ expat/tests/runtests.c
@@ -4400,7 +4400,7 @@ START_TEST(test_ignore_section_utf16)
         /* <d><e>&en;</e></d> */
         "<\0d\0>\0<\0e\0>\0&\0e\0n\0;\0<\0/\0e\0>\0<\0/\0d\0>\0";
     const XML_Char *expected =
-        "<![IGNORE[<!ELEMENT e (#PCDATA)*>]]>\n&en;";
+        XCS("<![IGNORE[<!ELEMENT e (#PCDATA)*>]]>\n&en;");
     CharData storage;
 
     CharData_Init(&storage);
