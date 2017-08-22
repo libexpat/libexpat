39a88acb01c8a86b38c85aa96965bf25aba46a3e
Rhodri James <rhodri@kynesim.co.uk>
2017-08-22 15:59:45 +0100

    Fix test_suspend_resume_internal_entity() to work for UTF-16

--- expat/tests/runtests.c
+++ expat/tests/runtests.c
@@ -5247,8 +5247,8 @@ START_TEST(test_suspend_resume_internal_entity)
         "<!ENTITY foo '<suspend>Hi<suspend>Ho</suspend></suspend>'>\n"
         "]>\n"
         "<doc>&foo;</doc>\n";
-    const char *expected1 = "Hi";
-    const char *expected2 = "HiHo";
+    const XML_Char *expected1 = XCS("Hi");
+    const XML_Char *expected2 = XCS("HiHo");
     CharData storage;
 
     CharData_Init(&storage);
@@ -5258,7 +5258,7 @@ START_TEST(test_suspend_resume_internal_entity)
     if (XML_Parse(parser, text, strlen(text),
                   XML_TRUE) != XML_STATUS_SUSPENDED)
         xml_failure(parser);
-    CharData_CheckXMLChars(&storage, "");
+    CharData_CheckXMLChars(&storage, XCS(""));
     if (XML_ResumeParser(parser) != XML_STATUS_SUSPENDED)
         xml_failure(parser);
     CharData_CheckXMLChars(&storage, expected1);
