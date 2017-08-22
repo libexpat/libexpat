ae47db828857fee0d4212285de2362c934f7f0da
Rhodri James <rhodri@kynesim.co.uk>
2017-08-22 16:57:03 +0100

    Fix test_ext_entity_latin1_utf16le_bom2() to work in UTF-16 builds

--- expat/tests/runtests.c
+++ expat/tests/runtests.c
@@ -6240,8 +6240,12 @@ START_TEST(test_ext_entity_latin1_utf16le_bom2)
         NULL,
         EE_PARSE_FULL_BUFFER
     };
+#ifdef XML_UNICODE
+    const XML_Char *expected = XCS("\x00ff\x00feL ");
+#else
     /* In UTF-8, y-diaeresis is 0xc3 0xbf, lowercase thorn is 0xc3 0xbe */
-    const XML_Char *expected = "\xc3\xbf\xc3\xbeL ";
+    const XML_Char *expected = XCS("\xc3\xbf\xc3\xbeL ");
+#endif
     CharData storage;
 
 
