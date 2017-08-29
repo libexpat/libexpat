fec399291ed7ac00def8fd3312b9d29493173c58
Rhodri James <rhodri@kynesim.co.uk>
2017-08-29 18:21:30 +0100

    Make xmltest.sh script work for UTF-16 builds

--- expat/tests/xmltest.sh
+++ expat/tests/xmltest.sh
@@ -31,6 +31,8 @@ TS="$MYDIR"
 # OUTPUT must terminate with the directory separator.
 OUTPUT="$TS/out/"
 # OUTPUT=/home/tmp/xml-testsuite-out/
+# Unicode-aware diff utility
+DIFF="$TS/udiffer.py"
 
 
 # RunXmlwfNotWF file reldir
@@ -57,7 +59,7 @@ RunXmlwfWF() {
   read outdata < outfile 
   if test "$outdata" = "" ; then 
       if [ -f "out/$file" ] ; then 
-          diff -u "$OUTPUT$reldir$file" "out/$file" > outfile 
+          $DIFF "$OUTPUT$reldir$file" "out/$file" > outfile 
           if [ -s outfile ] ; then 
               cp outfile "$OUTPUT$reldir$file.diff"
               echo "Output differs: $reldir$file"
