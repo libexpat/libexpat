f5322b01ac4d8f83ea60b2b61e5790e5a9ed18f3
Sebastian Pipping <sebastian@pipping.org>
2017-12-03 18:32:57 +0100

    Address 64bit intptr_t loss warning for Visual Studio
    Message was:
    warning C4244: '=' : conversion from 'intptr_t' to 'int', possible loss of data

--- expat/tests/runtests.c
+++ expat/tests/runtests.c
@@ -5050,7 +5050,7 @@ external_entity_devaluer(XML_Parser parser,
         "<!ENTITY % e1 SYSTEM 'bar'>\n"
         "%e1;\n";
     XML_Parser ext_parser;
-    int clear_handler = (intptr_t)XML_GetUserData(parser);
+    intptr_t clear_handler = (intptr_t)XML_GetUserData(parser);
 
     if (systemId == NULL || !xcstrcmp(systemId, XCS("bar")))
         return XML_STATUS_OK;
@@ -7865,8 +7865,8 @@ END_TEST
 #define ALLOC_ALWAYS_SUCCEED (-1)
 #define REALLOC_ALWAYS_SUCCEED (-1)
 
-static int allocation_count = ALLOC_ALWAYS_SUCCEED;
-static int reallocation_count = REALLOC_ALWAYS_SUCCEED;
+static intptr_t allocation_count = ALLOC_ALWAYS_SUCCEED;
+static intptr_t reallocation_count = REALLOC_ALWAYS_SUCCEED;
 
 /* Crocked allocator for allocation failure tests */
 static void *duff_allocator(size_t size)
