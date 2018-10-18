b9295ccb51622d520522fed34bd66ed505ebff24
Marco Maggi <marco.maggi-ipsu@poste.it>
2018-10-18 09:11:02 +0200

    added command line interface to xmltest.sh; better variable name in the same script

--- expat/tests/xmltest.sh
+++ expat/tests/xmltest.sh
@@ -26,23 +26,6 @@ shopt -s nullglob
 
 #### global variables
 
-# The environment  variable EXPAT_ABS_SRCDIR is  set in the  Makefile to
-# the source directory.  The  environment variable EXPAT_ABS_BUILDDIR is
-# set in the Makefile to the build directory.
-#
-declare -r EXPAT_ABS_SRCDIR="${EXPAT_ABS_SRCDIR:?'environment variable EXPAT_ABS_SRCDIR is not set'}"
-declare -r EXPAT_ABS_BUILDDIR="${EXPAT_ABS_BUILDDIR:?'environment variable EXPAT_ABS_BUILDDIR is not set'}"
-
-declare -r RUNNER="${1:-${EXPAT_ABS_BUILDDIR}/run.sh}"
-declare -r XMLWF="${2:-${EXPAT_ABS_BUILDDIR}/xmlwf/xmlwf}"
-
-# Unicode-aware diff utility.  Requires Python version 3.
-#
-declare -r DIFF="${EXPAT_ABS_SRCDIR}/tests/udiffer.py"
-
-declare -r XML_BASE_SRCDIR="${EXPAT_ABS_BUILDDIR}"/tests/xmlconf
-declare -r XML_BASE_DSTDIR="${EXPAT_ABS_BUILDDIR}"/tests/xmltest-output
-
 # Number of successful tests.
 #
 declare -i SUCCESS=0
@@ -51,14 +34,120 @@ declare -i SUCCESS=0
 #
 declare -i ERROR=0
 
+# Enable/disable verbose execution.
+#
+declare VERBOSE=false
 
 #### main function
 
 function main () {
+    local OPTNAME OPT_ABS_SRCDIR OPT_ABS_BUILDDIR OPT_RUNNER OPT_XMLWF
+    local OPT_ABS_XMLCONF OPT_ABS_OUTPUT
+
+    while getopts :vhS:B:R:X:T:O: OPTNAME
+    do
+        case ${OPTNAME} in
+            S)
+                OPT_ABS_SRCDIR="${OPTARG}"
+                ;;
+            B)
+                OPT_ABS_BUILDDIR="${OPTARG}"
+                ;;
+            R)
+                OPT_RUNNER="${OPTARG}"
+                ;;
+            X)
+                OPT_XMLWF="${OPTARG}"
+                ;;
+            T)
+                OPT_ABS_XMLCONF="${OPTARG}"
+                ;;
+            O)
+                OPT_ABS_OUTPUT="${OPTARG}"
+                ;;
+            h)
+                print_usage_screen_and_exit
+                ;;
+            v)
+                VERBOSE=true
+                ;;
+            \?)
+                print_error_message 'invalid option "-%s"' "${OPTARG}"
+                exit 1
+                ;;
+        esac
+    done
+
+    shift $((OPTIND - 1))
+    if test -n "$1"
+    then
+        print_error_message 'invalid command line argument "%s"' "$1"
+        exit 1
+    fi
+
+    # EXPAT_ABS_SRCDIR is  set to the  absolute pathname to  the "expat"
+    # source  directory.   EXPAT_ABS_BUILDDIR  is set  to  the  absolute
+    # pathname to the build directory in which we ran "configure".
+    #
+    declare -r EXPAT_ABS_SRCDIR="${OPT_ABS_SRCDIR:?'option variable OPT_ABS_SRCDIR is not set'}"
+    declare -r EXPAT_ABS_BUILDDIR="${OPT_ABS_BUILDDIR:?'option variable OPT_ABS_BUILDDIR is not set'}"
+
+    declare -r RUNNER="${OPT_RUNNER:-${EXPAT_ABS_BUILDDIR}/run.sh}"
+    declare -r XMLWF="${OPT_XMLWF:-${EXPAT_ABS_BUILDDIR}/xmlwf/xmlwf}"
+
+    declare -r ABS_TEST_SUITE_DIR="${OPT_ABS_XMLCONF:-${EXPAT_ABS_BUILDDIR}/tests/xmlconf}"
+    declare -r ABS_TESTS_OUTPUT_DIR="${OPT_ABS_OUTPUT:-${EXPAT_ABS_BUILDDIR}/tests/xmltest-output}"
+
+    # Unicode-aware diff utility.  Requires Python version 3.
+    #
+    declare -r DIFF="${EXPAT_ABS_SRCDIR}"/tests/udiffer.py
+
+    print_verbose_message 'configuration:
+  EXPAT_ABS_SRCDIR=%s
+  EXPAT_ABS_BUILDDIR=%s
+  RUNNER=%s
+  XMLWF=%s
+  ABS_TEST_SUITE_DIR=%s
+  ABS_TESTS_OUTPUT_DIR=%s\n' \
+                          "${EXPAT_ABS_SRCDIR}"        \
+                          "${EXPAT_ABS_BUILDDIR}"      \
+                          "${RUNNER}"                  \
+                          "${XMLWF}"                   \
+                          "${ABS_TEST_SUITE_DIR}"      \
+                          "${ABS_TESTS_OUTPUT_DIR}"
+
     test_well_formed_tests_cases
     test_not_well_formed_tests_cases
 
     printf 'Passed: %d\nFailed: %d\n' ${SUCCESS} ${ERROR}
+}
+
+function print_usage_screen_and_exit () {
+    printf 'Usage: xmltest.sh -S ABS_SRCDIR -B ABS_BUILDDIR [OPTIONS]
+Options:
+  -S ABS_SRCDIR
+       Select the absolute pathname to the "expat" source directory.
+  -B ABS_BUILDDIR
+       Select the absolute pathname to the build directory in which we
+       ran "configure".
+  -R RUNNER
+       Select the absolute pathname to "run.sh".
+       Defaults to: "$ABS_BUILDDIR/run.sh".
+  -X XMLWF
+       Select the absolute pathname to "xmlwf".
+       Defaults to: "$ABS_BUILDDIR/xmlwf/xmlwf".
+  -T XMLCONF
+       Select the absolute pathname to the directory "xmlconf" from the
+       downloaded XML test suite.
+       Defaults to: "$ABS_BUILDDIR/tests/xmlconf".
+  -O OUTPUT
+       Select the absolute pathname to the tests output directory.
+       Defaults to: "$ABS_BUILDDIR/tests/xmltest-output".
+  -v
+       Enable verbose execution.
+  -h
+       Print the usage screen and exit.\n' >&2
+    exit 0
 }
 
 
@@ -67,7 +156,7 @@ function main () {
 function test_well_formed_tests_cases () {
     local XML_RELATIVE_SRCDIR XML_FILE_NAME
 
-    cd "${XML_BASE_SRCDIR}" || return $?
+    cd "${ABS_TEST_SUITE_DIR}" || return $?
     for XML_RELATIVE_SRCDIR in \
         ibm/valid/P* \
             ibm/invalid/P* \
@@ -79,8 +168,8 @@ function test_well_formed_tests_cases () {
             sun/valid \
             sun/invalid
     do
-        make_directory "${XML_BASE_DSTDIR}/${XML_RELATIVE_SRCDIR}" || return $?
-        cd "${XML_BASE_SRCDIR}/${XML_RELATIVE_SRCDIR}" || return $?
+        make_directory "${ABS_TESTS_OUTPUT_DIR}/${XML_RELATIVE_SRCDIR}" || return $?
+        cd "${ABS_TEST_SUITE_DIR}/${XML_RELATIVE_SRCDIR}" || return $?
         for XML_FILE_NAME in $(ls -1 *.xml | sort -d)
         do
             [[ -f "${XML_FILE_NAME}" ]] || continue
@@ -90,9 +179,9 @@ function test_well_formed_tests_cases () {
     done
 
     XML_RELATIVE_SRCDIR=oasis
-    make_directory "${XML_BASE_DSTDIR}/${XML_RELATIVE_SRCDIR}" || return $?
+    make_directory "${ABS_TESTS_OUTPUT_DIR}/${XML_RELATIVE_SRCDIR}" || return $?
 
-    cd "${XML_BASE_SRCDIR}/${XML_RELATIVE_SRCDIR}" || return $?
+    cd "${ABS_TEST_SUITE_DIR}/${XML_RELATIVE_SRCDIR}" || return $?
     for XML_FILE_NAME in *pass*.xml ; do
         run_xmlwf_well_formed_doc_test "${XML_RELATIVE_SRCDIR}" "${XML_FILE_NAME}"
         update_test_result_status $?
@@ -105,7 +194,7 @@ function test_well_formed_tests_cases () {
 function test_not_well_formed_tests_cases () {
     local XML_RELATIVE_SRCDIR XML_FILE_NAME
 
-    cd "${XML_BASE_SRCDIR}" || return $?
+    cd "${ABS_TEST_SUITE_DIR}" || return $?
     for XML_RELATIVE_SRCDIR in \
         ibm/not-wf/P* \
             ibm/not-wf/p28a \
@@ -115,7 +204,7 @@ function test_not_well_formed_tests_cases () {
             xmltest/not-wf/sa \
             sun/not-wf
     do
-        cd "${XML_BASE_SRCDIR}/${XML_RELATIVE_SRCDIR}" || return $?
+        cd "${ABS_TEST_SUITE_DIR}/${XML_RELATIVE_SRCDIR}" || return $?
         for XML_FILE_NAME in *.xml
         do
             run_xmlwf_not_well_formed_doc_test "${XML_RELATIVE_SRCDIR}" "${XML_FILE_NAME}"
@@ -124,9 +213,9 @@ function test_not_well_formed_tests_cases () {
     done
 
     XML_RELATIVE_SRCDIR=oasis
-    make_directory "${XML_BASE_DSTDIR}/${XML_RELATIVE_SRCDIR}" || return $?
+    make_directory "${ABS_TESTS_OUTPUT_DIR}/${XML_RELATIVE_SRCDIR}" || return $?
 
-    cd "${XML_BASE_SRCDIR}/${XML_RELATIVE_SRCDIR}" || return $?
+    cd "${ABS_TEST_SUITE_DIR}/${XML_RELATIVE_SRCDIR}" || return $?
     for XML_FILE_NAME in *fail*.xml
     do
         run_xmlwf_not_well_formed_doc_test "${XML_RELATIVE_SRCDIR}" "${XML_FILE_NAME}"
@@ -139,13 +228,13 @@ function test_not_well_formed_tests_cases () {
 
 # Upon entering this function, the current working directory must be:
 #
-#   "${XML_BASE_SRCDIR}/${XML_RELATIVE_SRCDIR}"
+#   "${ABS_TEST_SUITE_DIR}/${XML_RELATIVE_SRCDIR}"
 #
 function run_xmlwf_not_well_formed_doc_test () {
     local -r XML_RELATIVE_SRCDIR="${1:?missing XML relative source directory pathname argument in call to ${FUNCNAME}}"
     local -r XML_FILE_NAME="${2:?missing XML file name argument in call to ${FUNCNAME}}"
 
-    local -r XMLWF_NOT_WELL_FORMED_LOG_DIR="${XML_BASE_DSTDIR}/${XML_RELATIVE_SRCDIR}"
+    local -r XMLWF_NOT_WELL_FORMED_LOG_DIR="${ABS_TESTS_OUTPUT_DIR}/${XML_RELATIVE_SRCDIR}"
     local -r XMLWF_NOT_WELL_FORMED_LOG_FILE="${XMLWF_NOT_WELL_FORMED_LOG_DIR}/${XML_FILE_NAME}.log"
     local NOT_WELL_FORMED_ERROR_DESCRIPTION
 
@@ -180,18 +269,18 @@ function run_xmlwf_not_well_formed_doc_test () {
 
 # Upon entering this function, the current working directory must be:
 #
-#   "${XML_BASE_SRCDIR}/${XML_RELATIVE_SRCDIR}"
+#   "${ABS_TEST_SUITE_DIR}/${XML_RELATIVE_SRCDIR}"
 #
 function run_xmlwf_well_formed_doc_test () {
     local -r XML_RELATIVE_SRCDIR="${1:?missing XML relative source directory pathname argument in call to ${FUNCNAME}}"
     local -r XML_FILE_NAME="${2:?missing XML file name argument in call to ${FUNCNAME}}"
 
-    local -r XMLWF_NOT_WELL_FORMED_LOG_DIR="${XML_BASE_DSTDIR}/${XML_RELATIVE_SRCDIR}"
+    local -r XMLWF_NOT_WELL_FORMED_LOG_DIR="${ABS_TESTS_OUTPUT_DIR}/${XML_RELATIVE_SRCDIR}"
     local -r XMLWF_NOT_WELL_FORMED_LOG_FILE="${XMLWF_NOT_WELL_FORMED_LOG_DIR}/${XML_FILE_NAME}.log"
     local NOT_WELL_FORMED_ERROR_DESCRIPTION
 
-    local -r XML_PRECOMPUTED_TRANSFORMED_PATHNAME="${XML_BASE_SRCDIR}/${XML_RELATIVE_SRCDIR}/out/${XML_FILE_NAME}"
-    local -r XMLWF_TRANSFORMED_OUTPUT_DIR="${XML_BASE_DSTDIR}/${XML_RELATIVE_SRCDIR}"
+    local -r XML_PRECOMPUTED_TRANSFORMED_PATHNAME="${ABS_TEST_SUITE_DIR}/${XML_RELATIVE_SRCDIR}/out/${XML_FILE_NAME}"
+    local -r XMLWF_TRANSFORMED_OUTPUT_DIR="${ABS_TESTS_OUTPUT_DIR}/${XML_RELATIVE_SRCDIR}"
     local -r XMLWF_TRANSFORMED_OUTPUT_FILE="${XMLWF_TRANSFORMED_OUTPUT_DIR}/${XML_FILE_NAME}"
     local -r XMLWF_TRANSFORMED_DIFF_FILE="${XMLWF_TRANSFORMED_OUTPUT_DIR}/${XML_FILE_NAME}.diff"
 
@@ -245,6 +334,14 @@ function print_error_message () {
     printf "xmltest.sh: ${TEMPLATE}\n" "$@" >&2
 }
 
+function print_verbose_message () {
+    local -r TEMPLATE="${1:?missing template argument in call to ${FUNCNAME}}"
+    shift
+    if ${VERBOSE}
+    then printf "xmltest.sh: ${TEMPLATE}\n" "$@" >&2
+    fi
+}
+
 function print_log_message () {
     local -r TEMPLATE="${1:?missing template argument in call to ${FUNCNAME}}"
     shift
@@ -272,6 +369,6 @@ function update_test_result_status () {
 
 #### done
 
-main
+main "$@"
 
 ### end of file
