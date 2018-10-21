4dc2104a146f9e7902b73c54749f46abaf15a5ff
Marco Maggi <marco.maggi-ipsu@poste.it>
2018-10-21 07:58:43 +0200

    removed unused variable definitions in coverage script

--- expat/coverage.sh
+++ expat/coverage.sh
@@ -102,9 +102,6 @@ _run() {
 
     ${with_unsigned_char} && BASE_FLAGS="${BASE_FLAGS} -funsigned-char"
 
-    local CFLAGS="-std=c99 ${BASE_FLAGS}"
-    local CXXFLAGS="-std=c++98 ${BASE_FLAGS}"
-
     (
         set -e
         cd "${build_dir}"
