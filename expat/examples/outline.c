36fa081cfe84b9edc9a638a0fccd38ae5bb85db9
Sebastian Pipping <sebastian@pipping.org>
2019-08-29 18:51:03 +0200

    Drop support for Visual Studio <=7.1/2003
    _MSC_VER 1400 is 8.0/2005

--- expat/examples/outline.c
+++ expat/examples/outline.c
@@ -36,11 +36,7 @@
 #include <expat.h>
 
 #ifdef XML_LARGE_SIZE
-#  if defined(XML_USE_MSC_EXTENSIONS) && _MSC_VER < 1400
-#    define XML_FMT_INT_MOD "I64"
-#  else
-#    define XML_FMT_INT_MOD "ll"
-#  endif
+#  define XML_FMT_INT_MOD "ll"
 #else
 #  define XML_FMT_INT_MOD "l"
 #endif
--- expat/xmlwf/xmlfile.h
+++ expat/xmlwf/xmlfile.h
@@ -34,11 +34,7 @@
 #define XML_EXTERNAL_ENTITIES 02
 
 #ifdef XML_LARGE_SIZE
-#  if defined(XML_USE_MSC_EXTENSIONS) && _MSC_VER < 1400
-#    define XML_FMT_INT_MOD "I64"
-#  else
-#    define XML_FMT_INT_MOD "ll"
-#  endif
+#  define XML_FMT_INT_MOD "ll"
 #else
 #  define XML_FMT_INT_MOD "l"
 #endif
