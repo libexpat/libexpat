ccd1cbc14db64f81f7404c5da37e1a9b9fa6de3b
Sebastian Pipping <sebastian@pipping.org>
2019-10-09 19:56:19 +0200

    Drop expat.spec RPM packaging (related to #330)

--- .travis.sh
+++ .travis.sh
@@ -45,10 +45,6 @@ cd expat
 if [[ ${MODE} = distcheck ]]; then
     ./configure ${CONFIGURE_ARGS}
     make distcheck
-
-    mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
-    ln -v -s "$PWD"/expat-*.tar.bz2 ~/rpmbuild/SOURCES/
-    rpmbuild -ba expat.spec
 elif [[ ${MODE} = cmake-oos ]]; then
     mkdir build
     cd build
