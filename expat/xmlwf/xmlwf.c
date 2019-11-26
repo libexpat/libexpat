83e1688c74ef4d9034e6b47a6595dee480be6147
Sebastian Pipping <sebastian@pipping.org>
2019-11-26 16:17:44 +0100

    xmlwf: Improve output of "xmlwf -h" by two tiny bits

--- expat/xmlwf/xmlwf.c
+++ expat/xmlwf/xmlwf.c
@@ -857,7 +857,7 @@ usage(const XML_Char *prog, int rc) {
       T("xmlwf - Determines if an XML document is well-formed\n")
       T("\n")
       T("positional arguments:\n")
-      T("  FILE          files to process (default: STDIN)\n")
+      T("  FILE          file to process (default: STDIN)\n")
       T("\n")
       T("input control arguments:\n")
       T("  -s            print an error if the document is not [s]tandalone\n")
@@ -879,7 +879,7 @@ usage(const XML_Char *prog, int rc) {
       T("  -h            show this [h]elp message and exit\n")
       T("  -v            show program's [v]ersion number and exit\n")
       T("\n")
-      T("libexpat is software libre, licensed under the MIT license.\n")
+      T("xmlwf of libexpat is software libre, licensed under the MIT license.\n")
       T("Please report bugs at https://github.com/libexpat/libexpat/issues.  Thank you!\n")
       , /* clang-format on */
       prog);
