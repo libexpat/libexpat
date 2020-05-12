d83fc3afd5e37d969d5b634a7b9b12ebec4428ee
Sebastian Pipping <sebastian@pipping.org>
2020-05-12 22:06:34 +0200

    Travis: Install a find(1) better than that of macOS

--- .travis.sh
+++ .travis.sh
@@ -34,6 +34,7 @@ if [[ ${TRAVIS_OS_NAME} = osx ]]; then
     latest_brew_python3_bin="$(ls -1d /usr/local/Cellar/python/3.*/bin | sort -n | tail -n1)"
     export PATH="${latest_brew_python3_bin}${PATH:+:}${PATH}"
     export PATH="/usr/local/opt/coreutils/libexec/gnubin${PATH:+:}${PATH}"
+    export PATH="/usr/local/opt/findutils/libexec/gnubin${PATH:+:}${PATH}"
 elif [[ ${TRAVIS_OS_NAME} = linux ]]; then
     export PATH="/usr/lib/llvm-9/bin:${PATH}"
 fi
