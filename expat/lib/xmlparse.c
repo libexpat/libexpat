857fdc4c3bf47eb3fedcd15d3763f62727476df0
Sebastian Pipping <sebastian@pipping.org>
2021-04-14 17:30:22 +0200

    lib: Add prefix "expat: " to EXPAT_ENTROPY_DEBUG=1 stderr output

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -889,7 +889,7 @@ static unsigned long
 ENTROPY_DEBUG(const char *label, unsigned long entropy) {
   const char *const EXPAT_ENTROPY_DEBUG = getenv("EXPAT_ENTROPY_DEBUG");
   if (EXPAT_ENTROPY_DEBUG && ! strcmp(EXPAT_ENTROPY_DEBUG, "1")) {
-    fprintf(stderr, "Entropy: %s --> 0x%0*lx (%lu bytes)\n", label,
+    fprintf(stderr, "expat: Entropy: %s --> 0x%0*lx (%lu bytes)\n", label,
             (int)sizeof(entropy) * 2, entropy, (unsigned long)sizeof(entropy));
   }
   return entropy;
