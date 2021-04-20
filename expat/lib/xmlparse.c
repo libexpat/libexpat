fcd0e14c3e35a56eb7ec42142a12e984fbe1b3c0
Sebastian Pipping <sebastian@pipping.org>
2021-04-20 14:01:27 +0200

    lib: Address Cppcheck 2.4.1 warning "uninitvar"

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -5581,7 +5581,8 @@ appendAttributeValue(XML_Parser parser, const ENCODING *enc, XML_Bool isCdata,
 #endif
 
   for (;;) {
-    const char *next;
+    const char *next
+        = ptr; /* XmlAttributeValueTok doesn't always set the last arg */
     int tok = XmlAttributeValueTok(enc, ptr, end, &next);
 #ifdef XML_DTD
     if (! accountingDiffTolerated(parser, tok, ptr, next, __LINE__, account)) {
@@ -5792,7 +5793,8 @@ storeEntityValue(XML_Parser parser, const ENCODING *enc,
   }
 
   for (;;) {
-    const char *next;
+    const char *next
+        = entityTextPtr; /* XmlEntityValueTok doesn't always set the last arg */
     int tok = XmlEntityValueTok(enc, entityTextPtr, entityTextEnd, &next);
 
 #ifdef XML_DTD
