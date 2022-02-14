b04b8022e8f1a89458ded12d29ee9541e11e87e5
Sebastian Pipping <sebastian@pipping.org>
2022-02-14 03:48:27 +0100

    lib: Add a multi-char version of poolAppendChar based on memcpy

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -583,6 +583,7 @@ static XML_Char *poolAppend(STRING_POOL *pool, const ENCODING *enc,
 static XML_Char *poolStoreString(STRING_POOL *pool, const ENCODING *enc,
                                  const char *ptr, const char *end);
 static XML_Bool FASTCALL poolGrow(STRING_POOL *pool);
+static XML_Bool FASTCALL poolGrowUntil(STRING_POOL *pool, size_t needed);
 static const XML_Char *FASTCALL poolCopyString(STRING_POOL *pool,
                                                const XML_Char *s);
 static const XML_Char *poolCopyStringN(STRING_POOL *pool, const XML_Char *s,
@@ -645,6 +646,11 @@ static unsigned long getDebugLevel(const char *variableName,
   (((pool)->ptr == (pool)->end && ! poolGrow(pool))                            \
        ? 0                                                                     \
        : ((*((pool)->ptr)++ = c), 1))
+#define poolAppendChars(pool, s, len)                                          \
+  (! poolGrowUntil((pool), (len))                                              \
+       ? 0                                                                     \
+       : (memcpy((pool)->ptr, (s), (len) * sizeof(XML_Char)),                  \
+          (pool)->ptr += (len), 1))
 
 #if ! defined(XML_TESTING)
 const
@@ -7837,6 +7843,19 @@ poolGrow(STRING_POOL *pool) {
     pool->end = tem->s + blockSize;
   }
   return XML_TRUE;
+}
+
+static XML_Bool FASTCALL
+poolGrowUntil(STRING_POOL *pool, size_t needed) {
+  for (;;) {
+    const size_t available = pool->end - pool->ptr;
+    if (available >= needed) {
+      return XML_TRUE;
+    }
+    if (! poolGrow(pool)) {
+      return XML_FALSE;
+    }
+  }
 }
 
 static int FASTCALL
