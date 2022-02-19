221a8ce65a40961e691d5b67a5e42e19b9dca5f9
Sebastian Pipping <sebastian@pipping.org>
2022-02-19 19:46:32 +0100

    lib|xmlwf: Leverage function `xcslen`

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -114,6 +114,7 @@
 #include "ascii.h"
 #include "expat.h"
 #include "siphash.h"
+#include "xcsinc.c"
 
 #if defined(HAVE_ARC4RANDOM)
 #  include "random_arc4random.h"
@@ -4151,15 +4152,24 @@ storeAtts(XML_Parser parser, const ENCODING *enc, const char *attStr,
     return XML_ERROR_NONE;
   prefixLen = 0;
   if (parser->m_ns_triplets && binding->prefix->name) {
-    while (binding->prefix->name[prefixLen++])
-      ; /* prefixLen includes null terminator */
+    const size_t candidateLen
+        = xcslen(binding->prefix->name) + /*null terminator*/ 1;
+    /* Detect and prevent integer overflow */
+    if (candidateLen > INT_MAX)
+      return XML_ERROR_NO_MEMORY;
+    prefixLen = (int)candidateLen;
   }
   tagNamePtr->localPart = localPart;
   tagNamePtr->uriLen = binding->uriLen;
   tagNamePtr->prefix = binding->prefix->name;
   tagNamePtr->prefixLen = prefixLen;
-  for (i = 0; localPart[i++];)
-    ; /* i includes null terminator */
+  {
+    const size_t candidateLen = xcslen(localPart) + /*null terminator*/ 1;
+    /* Detect and prevent integer overflow */
+    if (candidateLen > INT_MAX)
+      return XML_ERROR_NO_MEMORY;
+    i = (int)candidateLen;
+  }
 
   /* Detect and prevent integer overflow */
   if (binding->uriLen > INT_MAX - prefixLen
@@ -6084,9 +6094,7 @@ doProlog(XML_Parser parser, const ENCODING *enc, const char *s, const char *end,
           return XML_ERROR_NO_MEMORY;
         name = el->name;
         dtd->scaffold[myindex].name = name;
-        nameLen = 0;
-        while (name[nameLen++])
-          ;
+        nameLen = xcslen(name) + /*null terminator*/ 1;
 
         /* Detect and prevent integer overflow */
         if (nameLen > UINT_MAX - dtd->contentStringLen) {
@@ -7757,10 +7765,7 @@ keyeq(KEY s1, KEY s2) {
 
 static size_t
 keylen(KEY s) {
-  size_t len = 0;
-  for (; *s; s++, len++)
-    ;
-  return len;
+  return xcslen(s);
 }
 
 static void
@@ -8413,11 +8418,7 @@ copyString(const XML_Char *s, XML_Parser parser) {
   XML_Char *result;
 
   /* First determine how long the string is */
-  while (s[charsRequired] != 0) {
-    charsRequired++;
-  }
-  /* Include the terminator */
-  charsRequired++;
+  charsRequired = xcslen(s) + /*null terminator*/ 1;
 
   /* Now allocate space for the copy */
   result = MALLOC(parser, charsRequired * sizeof(XML_Char));
