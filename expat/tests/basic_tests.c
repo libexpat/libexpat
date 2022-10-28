5228e6e076491d6ad734e91a821208dab75ff5a2
Rhodri James <rhodri@wildebeest.org.uk>
2022-10-28 14:51:17 +0100

    Move first tranche of character/encoding tests out of runtests.c
    test_hash_collision is included to keep all the tests requiring
    siphash.h together.

--- expat/tests/basic_tests.c
+++ expat/tests/basic_tests.c
@@ -40,9 +40,12 @@
    USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+#include <string.h>
+
 #include "expat.h"
 #include "minicheck.h"
 #include "common.h"
+#include "siphash.h"
 #include "basic_tests.h"
 
 static void
@@ -52,12 +55,145 @@ basic_setup(void) {
     fail("Parser not created.");
 }
 
+/*
+ * Character & encoding tests.
+ */
+
+START_TEST(test_nul_byte) {
+  char text[] = "<doc>\0</doc>";
+
+  /* test that a NUL byte (in US-ASCII data) is an error */
+  if (_XML_Parse_SINGLE_BYTES(g_parser, text, sizeof(text) - 1, XML_TRUE)
+      == XML_STATUS_OK)
+    fail("Parser did not report error on NUL-byte.");
+  if (XML_GetErrorCode(g_parser) != XML_ERROR_INVALID_TOKEN)
+    xml_failure(g_parser);
+}
+END_TEST
+
+START_TEST(test_u0000_char) {
+  /* test that a NUL byte (in US-ASCII data) is an error */
+  expect_failure("<doc>&#0;</doc>", XML_ERROR_BAD_CHAR_REF,
+                 "Parser did not report error on NUL-byte.");
+}
+END_TEST
+
+START_TEST(test_siphash_self) {
+  if (! sip24_valid())
+    fail("SipHash self-test failed");
+}
+END_TEST
+
+START_TEST(test_siphash_spec) {
+  /* https://131002.net/siphash/siphash.pdf (page 19, "Test values") */
+  const char message[] = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09"
+                         "\x0a\x0b\x0c\x0d\x0e";
+  const size_t len = sizeof(message) - 1;
+  const uint64_t expected = SIP_ULL(0xa129ca61U, 0x49be45e5U);
+  struct siphash state;
+  struct sipkey key;
+
+  sip_tokey(&key, "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09"
+                  "\x0a\x0b\x0c\x0d\x0e\x0f");
+  sip24_init(&state, &key);
+
+  /* Cover spread across calls */
+  sip24_update(&state, message, 4);
+  sip24_update(&state, message + 4, len - 4);
+
+  /* Cover null length */
+  sip24_update(&state, message, 0);
+
+  if (sip24_final(&state) != expected)
+    fail("sip24_final failed spec test\n");
+
+  /* Cover wrapper */
+  if (siphash24(message, len, &key) != expected)
+    fail("siphash24 failed spec test\n");
+}
+END_TEST
+
+START_TEST(test_bom_utf8) {
+  /* This test is really just making sure we don't core on a UTF-8 BOM. */
+  const char *text = "\357\273\277<e/>";
+
+  if (_XML_Parse_SINGLE_BYTES(g_parser, text, (int)strlen(text), XML_TRUE)
+      == XML_STATUS_ERROR)
+    xml_failure(g_parser);
+}
+END_TEST
+
+START_TEST(test_bom_utf16_be) {
+  char text[] = "\376\377\0<\0e\0/\0>";
+
+  if (_XML_Parse_SINGLE_BYTES(g_parser, text, sizeof(text) - 1, XML_TRUE)
+      == XML_STATUS_ERROR)
+    xml_failure(g_parser);
+}
+END_TEST
+
+START_TEST(test_bom_utf16_le) {
+  char text[] = "\377\376<\0e\0/\0>\0";
+
+  if (_XML_Parse_SINGLE_BYTES(g_parser, text, sizeof(text) - 1, XML_TRUE)
+      == XML_STATUS_ERROR)
+    xml_failure(g_parser);
+}
+END_TEST
+
+/* Parse whole buffer at once to exercise a different code path */
+START_TEST(test_nobom_utf16_le) {
+  char text[] = " \0<\0e\0/\0>\0";
+
+  if (XML_Parse(g_parser, text, sizeof(text) - 1, XML_TRUE) == XML_STATUS_ERROR)
+    xml_failure(g_parser);
+}
+END_TEST
+
+START_TEST(test_hash_collision) {
+  /* For full coverage of the lookup routine, we need to ensure a
+   * hash collision even though we can only tell that we have one
+   * through breakpoint debugging or coverage statistics.  The
+   * following will cause a hash collision on machines with a 64-bit
+   * long type; others will have to experiment.  The full coverage
+   * tests invoked from qa.sh usually provide a hash collision, but
+   * not always.  This is an attempt to provide insurance.
+   */
+#define COLLIDING_HASH_SALT (unsigned long)SIP_ULL(0xffffffffU, 0xff99fc90U)
+  const char *text
+      = "<doc>\n"
+        "<a1/><a2/><a3/><a4/><a5/><a6/><a7/><a8/>\n"
+        "<b1></b1><b2 attr='foo'>This is a foo</b2><b3></b3><b4></b4>\n"
+        "<b5></b5><b6></b6><b7></b7><b8></b8>\n"
+        "<c1/><c2/><c3/><c4/><c5/><c6/><c7/><c8/>\n"
+        "<d1/><d2/><d3/><d4/><d5/><d6/><d7/>\n"
+        "<d8>This triggers the table growth and collides with b2</d8>\n"
+        "</doc>\n";
+
+  XML_SetHashSalt(g_parser, COLLIDING_HASH_SALT);
+  if (_XML_Parse_SINGLE_BYTES(g_parser, text, (int)strlen(text), XML_TRUE)
+      == XML_STATUS_ERROR)
+    xml_failure(g_parser);
+}
+END_TEST
+#undef COLLIDING_HASH_SALT
+
 TCase *
 make_basic_test_case(Suite *s) {
   TCase *tc_basic = tcase_create("basic tests");
 
   suite_add_tcase(s, tc_basic);
   tcase_add_checked_fixture(tc_basic, basic_setup, basic_teardown);
+
+  tcase_add_test(tc_basic, test_nul_byte);
+  tcase_add_test(tc_basic, test_u0000_char);
+  tcase_add_test(tc_basic, test_siphash_self);
+  tcase_add_test(tc_basic, test_siphash_spec);
+  tcase_add_test(tc_basic, test_bom_utf8);
+  tcase_add_test(tc_basic, test_bom_utf16_be);
+  tcase_add_test(tc_basic, test_bom_utf16_le);
+  tcase_add_test(tc_basic, test_nobom_utf16_le);
+  tcase_add_test(tc_basic, test_hash_collision);
 
   return tc_basic; /* TEMPORARY: this will become a void function */
 }
