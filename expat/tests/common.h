f555f08853a4056349b8b56fea4299f6c9463597
Rhodri James <rhodri@wildebeest.org.uk>
2022-10-28 12:52:51 +0100

    Move common test support functions to a separate file.
    A necessary first step if we are going to be able to reduce the
    size of runtests.c by splitting test cases into separate files.

---
+++ expat/tests/common.h
@@ -0,0 +1,103 @@
+/* Commonly used functions for the Expat test suite
+                            __  __            _
+                         ___\ \/ /_ __   __ _| |_
+                        / _ \\  /| '_ \ / _` | __|
+                       |  __//  \| |_) | (_| | |_
+                        \___/_/\_\ .__/ \__,_|\__|
+                                 |_| XML parser
+
+   Copyright (c) 2001-2006 Fred L. Drake, Jr. <fdrake@users.sourceforge.net>
+   Copyright (c) 2003      Greg Stein <gstein@users.sourceforge.net>
+   Copyright (c) 2005-2007 Steven Solie <steven@solie.ca>
+   Copyright (c) 2005-2012 Karl Waclawek <karl@waclawek.net>
+   Copyright (c) 2016-2022 Sebastian Pipping <sebastian@pipping.org>
+   Copyright (c) 2017-2022 Rhodri James <rhodri@wildebeest.org.uk>
+   Copyright (c) 2017      Joe Orton <jorton@redhat.com>
+   Copyright (c) 2017      José Gutiérrez de la Concha <jose@zeroc.com>
+   Copyright (c) 2018      Marco Maggi <marco.maggi-ipsu@poste.it>
+   Copyright (c) 2019      David Loffredo <loffredo@steptools.com>
+   Copyright (c) 2020      Tim Gates <tim.gates@iress.com>
+   Copyright (c) 2021      Dong-hee Na <donghee.na@python.org>
+   Licensed under the MIT license:
+
+   Permission is  hereby granted,  free of charge,  to any  person obtaining
+   a  copy  of  this  software   and  associated  documentation  files  (the
+   "Software"),  to  deal in  the  Software  without restriction,  including
+   without  limitation the  rights  to use,  copy,  modify, merge,  publish,
+   distribute, sublicense, and/or sell copies of the Software, and to permit
+   persons  to whom  the Software  is  furnished to  do so,  subject to  the
+   following conditions:
+
+   The above copyright  notice and this permission notice  shall be included
+   in all copies or substantial portions of the Software.
+
+   THE  SOFTWARE  IS  PROVIDED  "AS  IS",  WITHOUT  WARRANTY  OF  ANY  KIND,
+   EXPRESS  OR IMPLIED,  INCLUDING  BUT  NOT LIMITED  TO  THE WARRANTIES  OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+   NO EVENT SHALL THE AUTHORS OR  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+   DAMAGES OR  OTHER LIABILITY, WHETHER  IN AN  ACTION OF CONTRACT,  TORT OR
+   OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+   USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef XML_COMMON_H
+#  define XML_COMMON_H
+
+#  include "expat_config.h"
+#  include "minicheck.h"
+
+#  ifdef XML_LARGE_SIZE
+#    define XML_FMT_INT_MOD "ll"
+#  else
+#    define XML_FMT_INT_MOD "l"
+#  endif
+
+#  ifdef XML_UNICODE_WCHAR_T
+#    define XML_FMT_STR "ls"
+#    include <wchar.h>
+#    define xcstrlen(s) wcslen(s)
+#    define xcstrcmp(s, t) wcscmp((s), (t))
+#    define xcstrncmp(s, t, n) wcsncmp((s), (t), (n))
+#    define XCS(s) _XCS(s)
+#    define _XCS(s) L##s
+#  else
+#    ifdef XML_UNICODE
+#      error "No support for UTF-16 character without wchar_t in tests"
+#    else
+#      define XML_FMT_STR "s"
+#      define xcstrlen(s) strlen(s)
+#      define xcstrcmp(s, t) strcmp((s), (t))
+#      define xcstrncmp(s, t, n) strncmp((s), (t), (n))
+#      define XCS(s) s
+#    endif /* XML_UNICODE */
+#  endif   /* XML_UNICODE_WCHAR_T */
+
+extern XML_Parser g_parser;
+
+extern void tcase_add_test__ifdef_xml_dtd(TCase *tc, tcase_test_function test);
+
+extern void basic_teardown(void);
+
+extern void _xml_failure(XML_Parser parser, const char *file, int line);
+
+#  define xml_failure(parser) _xml_failure((parser), __FILE__, __LINE__)
+
+extern enum XML_Status _XML_Parse_SINGLE_BYTES(XML_Parser parser, const char *s,
+                                               int len, int isFinal);
+
+extern void _expect_failure(const char *text, enum XML_Error errorCode,
+                            const char *errorMessage, const char *file,
+                            int lineno);
+
+#  define expect_failure(text, errorCode, errorMessage)                        \
+    _expect_failure((text), (errorCode), (errorMessage), __FILE__, __LINE__)
+
+#endif /* XML_COMMON_H */
+
+#ifdef __cplusplus
+}
+#endif
