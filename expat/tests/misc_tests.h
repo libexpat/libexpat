36ea16c0c61e830e70a4124c79ce57d3d43b23f7
Donghee Na <donghee.na@python.org>
2023-09-23 01:11:23 +0900

    Update legal name of Donghee Na

--- expat/tests/misc_tests.h
+++ expat/tests/misc_tests.h
@@ -17,7 +17,7 @@
    Copyright (c) 2018      Marco Maggi <marco.maggi-ipsu@poste.it>
    Copyright (c) 2019      David Loffredo <loffredo@steptools.com>
    Copyright (c) 2020      Tim Gates <tim.gates@iress.com>
-   Copyright (c) 2021      Dong-hee Na <donghee.na@python.org>
+   Copyright (c) 2021      Donghee Na <donghee.na@python.org>
    Licensed under the MIT license:
 
    Permission is  hereby granted,  free of charge,  to any  person obtaining
