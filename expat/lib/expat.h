7554b5199e5895144da2d04e3af230c6f88aaab4
Sebastian Pipping <sebastian@pipping.org>
2025-09-15 17:14:02 +0200

    Bump version to 2.7.2

--- expat/lib/expat.h
+++ expat/lib/expat.h
@@ -1081,7 +1081,7 @@ XML_SetReparseDeferralEnabled(XML_Parser parser, XML_Bool enabled);
 */
 #  define XML_MAJOR_VERSION 2
 #  define XML_MINOR_VERSION 7
-#  define XML_MICRO_VERSION 1
+#  define XML_MICRO_VERSION 2
 
 #  ifdef __cplusplus
 }
