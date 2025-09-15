b8cd4e18e48af7c6736099769cf047f633e6af2c
Sebastian Pipping <sebastian@pipping.org>
2025-09-15 17:25:54 +0200

    Sync file headers

--- expat/xmlwf/unixfilemap.c
+++ expat/xmlwf/unixfilemap.c
@@ -10,7 +10,7 @@
    Copyright (c) 2000      Clark Cooper <coopercc@users.sourceforge.net>
    Copyright (c) 2001-2002 Fred L. Drake, Jr. <fdrake@users.sourceforge.net>
    Copyright (c) 2006      Karl Waclawek <karl@waclawek.net>
-   Copyright (c) 2016-2017 Sebastian Pipping <sebastian@pipping.org>
+   Copyright (c) 2016-2025 Sebastian Pipping <sebastian@pipping.org>
    Copyright (c) 2017      Rhodri James <rhodri@wildebeest.org.uk>
    Licensed under the MIT license:
 
