5b478ebfb4173d19f1038900bd727d9cc40fd2c8
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-04-15 17:12:45 +1000

    lib/xmlparse.c: Fix interpretation of 'errno' after 'getrandom'
    It would seem if this code uses `errno` alone to detect a `getrandom`
    failure, it needs to reset `errno` before the call. I.e. all values of
    `bytesWrittenMore` that do not set `success = 1` result in checking
    `errno != EINTR`. For an example of how this can go wrong:
    
      errno = ENOENT; // or call something else that sets errno != 0
      writeRandomBytes_getrandom_nonblock(foo, 257);
      // within writeRandomBytes_getrandom_nonblock:
      //   getrandom(…) returns 256
      //   loop is erroneously exited because errno == ENOENT
    
    This same issue appears to apply to `writeRandomBytes_dev_urandom` too.

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -1065,6 +1065,8 @@ writeRandomBytes_getrandom_nonblock(void *target, size_t count) {
 
     assert(bytesToWrite <= INT_MAX);
 
+    errno = 0;
+
     const int bytesWrittenMore =
 #      if defined(HAVE_GETRANDOM)
         (int)getrandom(currentTarget, bytesToWrite, getrandomFlags);
