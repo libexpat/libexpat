690a7c4cceb86c0ed424a0aaf1b30296858b3dca
Sebastian Pipping <sebastian@pipping.org>
2026-05-10 15:02:03 +0200

    Bump version to 2.8.1

--- expat/tests/misc_tests.c
+++ expat/tests/misc_tests.c
@@ -213,7 +213,7 @@ START_TEST(test_misc_version) {
   if (! versions_equal(&read_version, &parsed_version))
     fail("Version mismatch");
 
-  if (xcstrcmp(version_text, XCS("expat_2.8.0"))
+  if (xcstrcmp(version_text, XCS("expat_2.8.1"))
       != 0) /* needs bump on releases */
     fail("XML_*_VERSION in expat.h out of sync?\n");
 }
