43c70a56945d4ff1dc2bb4695277f41a17c999dd
Nick Begg <nick@stunttruck.net>
2026-05-16 15:32:09 +0200

    handle case when has_attribute doesn't exist

--- expat/lib/fallthrough.h
+++ expat/lib/fallthrough.h
@@ -2,7 +2,12 @@
 #define __EXPAT_FALLTHROUGH 1
 
 // Shamelessly taken from Python's pyport.h
-//
+#ifdef __has_attribute
+#  define __expat_has_attribute(x) __has_attribute(x)
+#else
+#  define __expat_has_attribute(x) 0
+#endif
+
 // Explicit fallthrough in switch case to avoid warnings
 // with compiler flag -Wimplicit-fallthrough.
 //
@@ -10,12 +15,14 @@
 // Apple Clang 12.0. Earlier Clang versions support only the C++11
 // style fallthrough attribute, not the GCC extension syntax used here,
 // and __has_attribute(fallthrough) evaluates to 1.
-#if __has_attribute(fallthrough) && (!defined(__clang__) || \
+#if __expat_has_attribute(fallthrough) && (!defined(__clang__) || \
     (!defined(__apple_build_version__) && __clang_major__ >= 10) || \
     (defined(__apple_build_version__) && __clang_major__ >= 12))
 #  define _EXPAT_FALLTHROUGH __attribute__((fallthrough))
 #else
 #  define _EXPAT_FALLTHROUGH do { } while (0)
 #endif
+
+#undef __expat_has_attribute
 
 #endif //__EXPAT_FALLTHROUGH
