d60a474d27ba19bb79a952451a8f845df8b6f8eb
Nick Begg <nick@stunttruck.net>
2026-05-17 15:22:04 +0200

    cleanup fallthrough.h slightly

--- expat/lib/fallthrough.h
+++ expat/lib/fallthrough.h
@@ -1,12 +1,5 @@
-#ifndef __EXPAT_FALLTHROUGH
-#  define __EXPAT_FALLTHROUGH 1
-
-// Shamelessly taken from Python's pyport.h
-#  ifdef __has_attribute
-#    define __expat_has_attribute(x) __has_attribute(x)
-#  else
-#    define __expat_has_attribute(x) 0
-#  endif
+#ifndef __FALLTHROUGH
+#  define __FALLTHROUGH 1
 
 // Explicit fallthrough in switch case to avoid warnings
 // with compiler flag -Wimplicit-fallthrough.
@@ -15,7 +8,7 @@
 // Apple Clang 12.0. Earlier Clang versions support only the C++11
 // style fallthrough attribute, not the GCC extension syntax used here,
 // and __has_attribute(fallthrough) evaluates to 1.
-#  if __expat_has_attribute(fallthrough)                                       \
+#  if defined(__has_attribute)                                                 \
       && (! defined(__clang__)                                                 \
           || (! defined(__apple_build_version__) && __clang_major__ >= 10)     \
           || (defined(__apple_build_version__) && __clang_major__ >= 12))
@@ -26,6 +19,4 @@
       } while (0)
 #  endif
 
-#  undef __expat_has_attribute
-
-#endif //__EXPAT_FALLTHROUGH
+#endif //__FALLTHROUGH
