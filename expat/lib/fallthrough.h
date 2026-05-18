371aee3ad993eca91d6dbf4a00a866abd8c398be
Nick Begg <nick@stunttruck.net>
2026-05-18 03:59:09 +0200

    Update fallthrough to check for >= C23 when using [[ ]] attributes.

--- expat/lib/fallthrough.h
+++ expat/lib/fallthrough.h
@@ -3,8 +3,9 @@
 
 // Explicit fallthrough in switch case to avoid warnings
 // with compiler flag -Wimplicit-fallthrough.
-
-#  if defined(__has_c_attribute) && __has_c_attribute(fallthrough)
+// [[ ]] attributes are a C23 feature
+#  if (__STDC_VERSION__ >= 202311L) && defined(__has_c_attribute)              \
+      && __has_c_attribute(fallthrough)
 #    define _EXPAT_FALLTHROUGH [[fallthrough]]
 #  elif defined(__has_attribute) && __has_attribute(fallthrough)
 #    define _EXPAT_FALLTHROUGH __attribute__((fallthrough))
