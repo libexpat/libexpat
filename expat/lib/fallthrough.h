1224a62f98afe34242122d88307c6a3e31d65c91
Nick Begg <nick@stunttruck.net>
2026-05-18 04:16:31 +0200

    Hopefully fix breakage from last change - case where __has_atribute is not defined

--- expat/lib/fallthrough.h
+++ expat/lib/fallthrough.h
@@ -4,12 +4,17 @@
 // Explicit fallthrough in switch case to avoid warnings
 // with compiler flag -Wimplicit-fallthrough.
 // [[ ]] attributes are a C23 feature
-#  if (__STDC_VERSION__ >= 202311L) && defined(__has_c_attribute)              \
-      && __has_c_attribute(fallthrough)
-#    define _EXPAT_FALLTHROUGH [[fallthrough]]
-#  elif defined(__has_attribute) && __has_attribute(fallthrough)
-#    define _EXPAT_FALLTHROUGH __attribute__((fallthrough))
-#  else
+#  if (__STDC_VERSION__ >= 202311L) && defined(__has_c_attribute)
+#    if __has_c_attribute(fallthrough)
+#      define _EXPAT_FALLTHROUGH [[fallthrough]]
+#    endif
+#  endif
+#  if ! defined(_EXPAT_FALLTHROUGH) && defined(__has_attribute)
+#    if __has_attribute(fallthrough)
+#      define _EXPAT_FALLTHROUGH __attribute__((fallthrough))
+#    endif
+#  endif
+#  if ! defined(_EXPAT_FALLTHROUGH)
 #    define _EXPAT_FALLTHROUGH                                                 \
       do {                                                                     \
       } while (0)
