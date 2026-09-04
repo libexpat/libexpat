0248d42f6983aa32b05cc787cfb3cea7a57214d4
Sebastian Pipping <sebastian@pipping.org>
2026-09-04 22:12:28 +0200

    [WIP] tests: Start covering properties API operation

--- expat/tests/props_tests.c
+++ expat/tests/props_tests.c
@@ -33,10 +33,152 @@ __  __            _
 
 #include "props_tests.h"
 
-#include "minicheck.h"
+#include "common.h" // for g_chunkSize
+#include "expat.h"
+#include "internal.h" // for e.g. EXPAT_ALLOC_TRACKER_ACTIVATION_THRESHOLD_DEFAULT
+#include "expat.h"
+
+START_TEST(test_props_getter_defaults) {
+  // The test is not doing any parsing, so a single run
+  // (with `g_chunkSize == 0`) is enough
+  if (g_chunkSize != 0)
+    return;
+
+  XML_Parser parser = XML_ParserCreate(NULL);
+
+#if XML_GE == 1
+  // Case XML_PROP_ALLOC_TRACKER_ACTIVATION_THRESHOLD
+  {
+    uint64_t actionThresholdBytes1 = 123;
+    assert_true(XML_GetPropertyUInt64(
+                    parser, XML_PROP_ALLOC_TRACKER_ACTIVATION_THRESHOLD,
+                    &actionThresholdBytes1)
+                == XML_PROP_ERROR_NONE);
+    assert_true(actionThresholdBytes1
+                == EXPAT_ALLOC_TRACKER_ACTIVATION_THRESHOLD_DEFAULT);
+  }
+
+  // Case XML_PROP_ALLOC_TRACKER_MAXIMUM_AMPLIFICATION
+  {
+    double maximumAmplification1 = 123.456;
+    assert_true(XML_GetPropertyDouble(
+                    parser, XML_PROP_ALLOC_TRACKER_MAXIMUM_AMPLIFICATION,
+                    &maximumAmplification1)
+                == XML_PROP_ERROR_NONE);
+    assert_true(maximumAmplification1
+                == EXPAT_ALLOC_TRACKER_MAXIMUM_AMPLIFICATION_DEFAULT);
+  }
+
+  // Case XML_PROP_BILLION_LAUGHS_ACTIVATION_THRESHOLD
+  {
+    uint64_t actionThresholdBytes2 = 123;
+    assert_true(XML_GetPropertyUInt64(
+                    parser, XML_PROP_BILLION_LAUGHS_ACTIVATION_THRESHOLD,
+                    &actionThresholdBytes2)
+                == XML_PROP_ERROR_NONE);
+    assert_true(
+        actionThresholdBytes2
+        == EXPAT_BILLION_LAUGHS_ATTACK_PROTECTION_ACTIVATION_THRESHOLD_DEFAULT);
+  }
+
+  // Case XML_PROP_ALLOC_TRACKER_MAXIMUM_AMPLIFICATION
+  {
+    double maximumAmplification2 = 123.456;
+    assert_true(XML_GetPropertyDouble(
+                    parser, XML_PROP_BILLION_LAUGHS_MAXIMUM_AMPLIFICATION,
+                    &maximumAmplification2)
+                == XML_PROP_ERROR_NONE);
+    assert_true(
+        maximumAmplification2
+        == EXPAT_BILLION_LAUGHS_ATTACK_PROTECTION_MAXIMUM_AMPLIFICATION_DEFAULT);
+  }
+#endif
+
+  // Case XML_PROP_REPARSE_DEFERRAL_ENABLED
+  XML_Bool reparseDeferralEnabled = XML_FALSE;
+  assert_true(XML_GetPropertyBool(parser, XML_PROP_REPARSE_DEFERRAL_ENABLED,
+                                  &reparseDeferralEnabled)
+              == XML_PROP_ERROR_NONE);
+  assert_true(reparseDeferralEnabled == g_reparseDeferralEnabledDefault);
+
+  // Case XML_PROP_INVALID
+  XML_Bool dummyBool = XML_FALSE;
+  double dummyDouble = 123.456;
+  uint64_t dummyUInt64 = 123;
+  assert_true(XML_GetPropertyBool(parser, XML_PROP_INVALID, &dummyBool)
+              == XML_PROP_ERROR_INVALID_KEY);
+  assert_true(XML_GetPropertyDouble(parser, XML_PROP_INVALID, &dummyDouble)
+              == XML_PROP_ERROR_INVALID_KEY);
+  assert_true(XML_GetPropertyUInt64(parser, XML_PROP_INVALID, &dummyUInt64)
+              == XML_PROP_ERROR_INVALID_KEY);
+
+  XML_ParserFree(parser);
+}
+END_TEST
+
+START_TEST(test_props_getter_error_parser_null) {
+  // The test is not doing any parsing, so a single run
+  // (with `g_chunkSize == 0`) is enough
+  if (g_chunkSize != 0)
+    return;
+
+  XML_Parser parserNonNull = XML_ParserCreate(NULL);
+  XML_Parser parsers[] = {parserNonNull, NULL};
+
+  for (size_t i = 0; i < sizeof(parsers) / sizeof(parsers[0]); i++) {
+    XML_Parser parser = parsers[i];
+    const enum XML_Prop_Error validKeyExpectedError
+        = (parser == NULL) ? XML_PROP_ERROR_PARSER_NULL : XML_PROP_ERROR_NONE;
+    // NOTE: Currently the parser is checked for being `NULL` before the key is
+    //       being checked for being valid.
+    //       That precedence among errors is not considered part of the API
+    //       contract: either error would be fine to return.
+    const enum XML_Prop_Error invalidKeyExpectedError
+        = (parser == NULL) ? XML_PROP_ERROR_PARSER_NULL
+                           : XML_PROP_ERROR_INVALID_KEY;
+
+    XML_Bool dummyBool = XML_FALSE;
+#if XML_GE == 1
+    double dummyDouble = 123.456;
+    uint64_t dummyUInt64 = 123;
+
+    assert_true(
+        XML_GetPropertyUInt64(
+            parser, XML_PROP_ALLOC_TRACKER_ACTIVATION_THRESHOLD, &dummyUInt64)
+        == validKeyExpectedError);
+    assert_true(
+        XML_GetPropertyDouble(
+            parser, XML_PROP_ALLOC_TRACKER_MAXIMUM_AMPLIFICATION, &dummyDouble)
+        == validKeyExpectedError);
+    assert_true(
+        XML_GetPropertyUInt64(
+            parser, XML_PROP_BILLION_LAUGHS_ACTIVATION_THRESHOLD, &dummyUInt64)
+        == validKeyExpectedError);
+    assert_true(
+        XML_GetPropertyDouble(
+            parser, XML_PROP_BILLION_LAUGHS_MAXIMUM_AMPLIFICATION, &dummyDouble)
+        == validKeyExpectedError);
+#endif
+    assert_true(XML_GetPropertyBool(parser, XML_PROP_REPARSE_DEFERRAL_ENABLED,
+                                    &dummyBool)
+                == validKeyExpectedError);
+
+    assert_true(XML_GetPropertyBool(parser, XML_PROP_INVALID, &dummyBool)
+                == invalidKeyExpectedError);
+    assert_true(XML_GetPropertyDouble(parser, XML_PROP_INVALID, &dummyDouble)
+                == invalidKeyExpectedError);
+    assert_true(XML_GetPropertyUInt64(parser, XML_PROP_INVALID, &dummyUInt64)
+                == invalidKeyExpectedError);
+  }
+
+  XML_ParserFree(parserNonNull);
+}
+END_TEST
 
 void
 make_props_test_case(Suite *s) {
   TCase *const tc_props = tcase_create("properties tests");
   suite_add_tcase(s, tc_props);
+  tcase_add_test(tc_props, test_props_getter_defaults);
+  tcase_add_test(tc_props, test_props_getter_error_parser_null);
 }
